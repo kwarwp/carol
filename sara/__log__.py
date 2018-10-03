cfc26a865681491f8e69783e3a30b8a446286527
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-03 18:29:01 -0300

    Created sara/__log__.py at Timestamp: 2018-10-03 21:29:01

---
+++ sara/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Oct 03 2018 18:28:58.743 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from _spy.vittoline.mainimport Cena,Elemento,Texto
+                                     ^
+SyntaxError: invalid syntax
+'''},
