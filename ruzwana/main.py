526afd5db7352f3aafb01d735664de5d6b23cc17
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-03 18:28:53 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-03 21:28:52

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,2 +1,4 @@
 #Cibele
 from _spy.vittolino.main import Cena, Elemento, Texto
+SEA ="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/The_deep_blue_sea_%286834127561%29.jpg/800px-The_deep_blue_sea_%286834127561%29.jpg"
+FISH ="https://www.freeiconspng.com/uploads/fish-png-transparent-15.png"
