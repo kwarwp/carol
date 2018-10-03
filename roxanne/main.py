72873a62bc698a4fc40078310a855bdcf78c7c8a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-03 18:28:43 -0300

    Saved roxanne/main.py at Timestamp: 2018-10-03 21:28:43

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,4 +1,4 @@
 #davy
 from _spy.vittolino.main import Cena,Elemento,Texto
 PARQUE ="https://cdn.civitatis.com/italia/milan/guia/parque-sempione.jpg"
-NINJA ="https://banner2.kisspng.com/20180213/vue/kisspng-teenage-mutant-ninja-turtles-samurai-shuriken-japanese-ninja-bodyguard-warrior-picture-5a8272c2078963.3823498015184984980309.jpg"
+NINJA =""
