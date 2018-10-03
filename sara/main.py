0786d033c9020f7d3bb4ac611d84dbddd433a993
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-03 18:27:09 -0300

    Saved sara/main.py at Timestamp: 2018-10-03 21:27:08

--- sara/main.py
+++ sara/main.py
@@ -1,4 +1,4 @@
 #lara 
 from _spy.vittoline.mainimport Cena,Elemento,Texto
 CASTELO ="https://http2.mlstatic.com/painel-sublimado-castelo-da-cinderela-20-x-30-sem-emenda-D_NQ_NP_881065-MLB25608613215_052017-F.jpg"
-PERSONAGEM"https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjZvYT4mevdAhVCTZAKHYZYB6MQjRx6BAgBEAU&url=http%3A%2F%2Fsoseriadosdetv.com%2F2017%2F08%2F21%2F9a-temporada-de-dpa-detetives-do-predio-azul-estreia-hoje%2F&psig=AOvVaw3z7n6wHO8Z-qh6TbpOrtuH&ust=1538687983792145"
+PERSONAGEM ="http://soseriadosdetv.com/wp-content/uploads/2017/08/dpa-detetives-do-predio-azul-9-temporada.png"
