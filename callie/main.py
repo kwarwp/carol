fda936b194f01c9f9e9d8d6e9388b5437d25d27f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-03 18:24:31 -0300

    Created callie/main.py at Timestamp: 2018-10-03 21:24:31

---
+++ callie/main.py
@@ -0,0 +1,4 @@
+# carlos eduardo de oliveira assumpcao
+from _spy.vittolino.main import Cena, Elemento, Texto
+IMGOD ="http://3.bp.blogspot.com/-PSaNS37veEI/UqkOmMBXcvI/AAAAAAAAAIQ/Nqv0XHO5fKk/s1600/Sonic.exe4.jpg"
+SONICEXE ="http://pixelartmaker.com/art/5c608d29642e09e.png"
