c977acf1eced5b4296ce85b2cbb8840acf6863d5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-03 18:29:28 -0300

    Saved courtney/main.py at Timestamp: 2018-10-03 21:29:27

--- courtney/main.py
+++ courtney/main.py
@@ -1,4 +1,7 @@
 # carol.courtney.main.py
 # yasmin.
 from _spy.vittolino.main import Cena,Elemento,Texto
-CASTELO ="https://ae01.alicdn.com/kf/HTB17VXqRVXXXXbGXpXXq6xXFXXX8/2017-3D-Yeni-5D-Elmas-Nak-Manzara-K-Kar-Kale-Bah-e-Evi-mazayka-Elmas-Mozaik.jpg_640x640.jpg"
+CASTELO ="https://ae01.alicdn.com/kf/HTB17VXqRVXXXXbGXpXXq6xXFXXX8/2017-3D-Yeni-5D-Elmas-Nak-Manzara-K-Kar-Kale-Bah-e-Evi-mazayka-Elmas-Mozaik.jpg_640x640.jpg""
+PERSONAGEM"https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjBtfnOm-vdAhVIPJAKHVvMAKEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D80czcfCpq9Y&psig=AOvVaw13g6nCRnfdwWPBulkirMUE&ust=1538688343731260"
+
+
