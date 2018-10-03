386d2fb4e2a290f8383165584b911ec04e1b2031
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-03 18:09:23 -0300

    Created sara/main.py at Timestamp: 2018-10-03 21:09:23

---
+++ sara/main.py
@@ -0,0 +1,3 @@
+#lara 
+from _spy.vittoline.mainimport Cena,Elemento,Texto
+CASTELO ="https://http2.mlstatic.com/painel-sublimado-castelo-da-cinderela-20-x-30-sem-emenda-D_NQ_NP_881065-MLB25608613215_052017-F.jpg"
