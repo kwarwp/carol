7621214c7f658a456ec447b536a471d40cf0d99e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-03 18:28:10 -0300

    Saved kathryn/main.py at Timestamp: 2018-10-03 21:28:07

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,4 +1,3 @@
 #Samuelbarros
 from _spy.vittolino.main import Cena,Elemento, Texto 
-PRAIA ="https://ogimg.infoglobo.com.br/boa-viagem/23123046-813-45f/GEOMIDIA/375/xwater-3092908_1920.jpg.pagespeed.ic.7q4TE_WU0u.jpg"
-
+PRAIA ="https://ogimg.infoglobo.com.br/boa-viagem/23123046-813-45f/GEOMIDIA/375/xwater-3092908_1920.jpg.pagespeed.ic.7q4TE_WU0u.jpg"'
