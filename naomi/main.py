7ba6faedf6ccad889a3ee347c8146fb5f275e837
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 10:20:11 -0300

    Saved naomi/main.py at Timestamp: 2018-10-05 13:20:10

--- naomi/main.py
+++ naomi/main.py
@@ -1,1 +1,3 @@
-# carol.naomi.main.py
+#luciane
+from _spy.vittolino.main import Cena, Elemento, Texto
+Montanha = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_aS8dmrmH4G4fNjNAGtiyFaa6XT8VvQ3u1jbEDMIguhty0gYH"
