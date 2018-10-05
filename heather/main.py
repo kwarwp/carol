a1ab90dc22a2a1a00a07741194acde95cb2aa44d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:33:41 -0300

    Saved heather/main.py at Timestamp: 2018-10-05 14:33:41

--- heather/main.py
+++ heather/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 
 PRANCHA ="https://s3.amazonaws.com/images.shaperbuddy.com/43/surfboards/27100185252f23.png"
 
-def HISTORIA():
+def Historia():
     PRAIA = Cena(img = "https://static1.squarespace.com/static/595257a1e110eb8ad5afc365/59710c1137c5815bfac4bc6b/59710cd4cd39c303e0182262/1500581128697/Praia97.jpg")
     PRAIA.vai()
 Historia()
