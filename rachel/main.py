26cdf9c092af932fe7ebfdb5f25311d07ab9e41a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 10:29:40 -0300

    Created rachel/main.py at Timestamp: 2018-10-05 13:29:40

---
+++ rachel/main.py
@@ -0,0 +1,3 @@
+# maria
+from _spy.vittolino.main import Cena, Elemento, Texto
+CASADEBONECA ="https://img01-olxpt.akamaized.net/img-olxpt/930449724_1_644x461_casa-de-bonecas-em-madeira-macia-novo-porto_rev001.jpg"
