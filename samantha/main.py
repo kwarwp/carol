0da0e66a51900555afc9cc07beed25856b899ec8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:02:13 -0300

    Saved samantha/main.py at Timestamp: 2018-10-05 14:02:13

--- samantha/main.py
+++ samantha/main.py
@@ -1,3 +1,6 @@
 #Lana
 from _spy.vittolino.main import Cena, Elemento, Texto
 RUA ="https://cdn.pixabay.com/photo/2016/01/19/17/43/traffic-1149857_960_720.jpg"
+https://pngquant.org/Ducati_side_shadow.png
+
+def Historia():
