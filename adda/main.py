67e0ae3c4366c99eb90b05153de9157d4bad8bbe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 10:47:12 -0300

    Saved adda/main.py at Timestamp: 2018-10-05 13:47:12

--- adda/main.py
+++ adda/main.py
@@ -1,3 +1,4 @@
 # lucimara
 from _ spy.vittolino.main import Cena, Elemento, Texto
 PRAIA = "https://www.fundospaisagens.com/Imagens/fundo-de-ecra-de-praia.jpg"
+SIRI ="https://i.pinimg.com/originals/ff/00/51/ff005199bfa12a152819f86b196a6037.png"
