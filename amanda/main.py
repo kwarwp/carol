4b9846ab2505a76b92b6451f74f752de48e48491
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:23:49 -0300

    Saved amanda/main.py at Timestamp: 2018-10-05 14:23:48

--- amanda/main.py
+++ amanda/main.py
@@ -1,6 +1,8 @@
 # luana cristina
-from _spy.vittolino.main import Cena,Elemento,Texto
+from _spy.vitollino.main import Cena,Elemento,Texto
 
 HOMEM = "https://1.bp.blogspot.com/-RilnuddcuWQ/Wc2BC-eYG8I/AAAAAAAABGs/tLwWjcNOig4gxl7RGF6lN-uPKrLVoKPqQCLcBGAs/s320/arvore_que_abraca%2B2.png"
 def Historia():
     ARVORES = Cena(img = "https://pensamentoverde.com.br/wp-content/uploads/2016/11/1-arvore.jpg")
+    ARVORES.vai()
+Historia()
