4ae4fcddd2a4bcbb7b30984f6afcd346e435aa38
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:19:34 -0300

    Saved heather/main.py at Timestamp: 2018-10-05 14:19:34

--- heather/main.py
+++ heather/main.py
@@ -5,3 +5,5 @@ PRANCHA ="https://s3.amazonaws.com/images.shaperbuddy.com/43/surfboards/27100185
 
 def HISTORIA():
     PRAIA = Cena(img = "https://static1.squarespace.com/static/595257a1e110eb8ad5afc365/59710c1137c5815bfac4bc6b/59710cd4cd39c303e0182262/1500581128697/Praia97.jpg")
+    PRAIA.vai()
+Historia()
