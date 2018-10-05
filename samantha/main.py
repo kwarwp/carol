113ef3b939de194c7234f911cc99b777dfcba611
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:25:32 -0300

    Saved samantha/main.py at Timestamp: 2018-10-05 14:25:31

--- samantha/main.py
+++ samantha/main.py
@@ -1,6 +1,9 @@
 #Lana
 from _spy.vittolino.main import Cena, Elemento, Texto
 RUA ="https://cdn.pixabay.com/photo/2016/01/19/17/43/traffic-1149857_960_720.jpg"
-https://pngquant.org/Ducati_side_shadow.png
+MOTO ="https://pngquant.org/Ducati_side_shadow.png"
 
 def Historia():
+    RUA = Cena(img = "https://cdn.pixabay.com/photo/2016/01/19/17/43/traffic-1149857_960_720.jpg")]
+    RUA.vai()
+historia()
