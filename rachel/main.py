941f1066ac925ffd705cac36043192a566cd0eaa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:23:55 -0300

    Saved rachel/main.py at Timestamp: 2018-10-05 14:23:55

--- rachel/main.py
+++ rachel/main.py
@@ -1,7 +1,8 @@
 # maria
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vttollino.main import Cena, Elemento, Texto
 
 BONECA ="https://images-americanas.b2w.io/produtos/01/00/item/128261/5/128261584_1GG.jpg"
 def Historia() :
     CASADEBONECA = Cena (img = "https://img01-olxpt.akamaized.net/img-olxpt/930449724_1_644x461_casa-de-bonecas-em-madeira-macia-novo-porto_rev001.jpg")
-    
+    CASADEBONECA.vai() 
+Historia()    
