f917b755d7ba67f9659f693a847e9fb07478876e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:52:42 -0300

    Saved naomi/main.py at Timestamp: 2018-10-05 14:52:42

--- naomi/main.py
+++ naomi/main.py
@@ -5,5 +5,6 @@ LEAO = "http://lionsbrusquebercofiacao.org.br/wp-content/uploads/2015/12/logo-le
 
 def Historia():
     MONTANHA = Cena(img = "https://www.fundoswiki.com/Uploads/fundoswiki.com/ImagensGrandes/paisagem-montanha-9.jpg") 
+    leao = Elemento(img=LEAO, tit="leao", style=dict(left=150, top=60, width=60, heigth=200)
     MONTANHA.vai()
 Historia()
