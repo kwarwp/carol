80e9b48e96afdbfed090298385381fd808d3b8e5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 10:25:13 -0300

    Saved naomi/main.py at Timestamp: 2018-10-05 13:25:13

--- naomi/main.py
+++ naomi/main.py
@@ -1,3 +1,3 @@
 #luciane
 from _spy.vittolino.main import Cena, Elemento, Texto
-Montanha = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_aS8dmrmH4G4fNjNAGtiyFaa6XT8VvQ3u1jbEDMIguhty0gYH"
+MONTANHA = "https://www.fundoswiki.com/Uploads/fundoswiki.com/ImagensGrandes/paisagem-montanha-9.jpg"
