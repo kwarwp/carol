2724c0e2368e84dc2d7e98b257c94660c9898a91
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 10:48:13 -0300

    Saved heather/main.py at Timestamp: 2018-10-05 13:48:13

--- heather/main.py
+++ heather/main.py
@@ -1,4 +1,4 @@
 #Heather
 from _spy.vittolino.main import Cena, Elemento, Texto
 PRAIA ="https://static1.squarespace.com/static/595257a1e110eb8ad5afc365/59710c1137c5815bfac4bc6b/59710cd4cd39c303e0182262/1500581128697/Praia97.jpg"
-
+PRANCHA ="https://s3.amazonaws.com/images.shaperbuddy.com/43/surfboards/27100185252f23.png"
