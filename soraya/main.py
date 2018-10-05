ab3a7ac64a179f53f8b33c2c6146023ee75d5195
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 10:46:53 -0300

    Created soraya/main.py at Timestamp: 2018-10-05 13:46:53

---
+++ soraya/main.py
@@ -0,0 +1,4 @@
+# carol.soraya.main.py
+from_spy.vittolino.main import Cena, Elemento, texto
+DESERTO = "https://img00.deviantart.net/0a32/i/2013/261/0/0/campo_de_guerra_ninja_naruto_by_lwisf3rxd-d6msb9n.png"
+SUNAGAURE = https://c1.staticflickr.com/4/3096/2913346696_9e9d1b3fa4.jpg
