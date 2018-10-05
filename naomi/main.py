be694d21b4f391ff0df6c093c948e8801d1ffa47
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:01:56 -0300

    Saved naomi/main.py at Timestamp: 2018-10-05 14:01:56

--- naomi/main.py
+++ naomi/main.py
@@ -2,3 +2,6 @@
 from _spy.vittolino.main import Cena, Elemento, Texto
 MONTANHA = "https://www.fundoswiki.com/Uploads/fundoswiki.com/ImagensGrandes/paisagem-montanha-9.jpg"
 LEAO = "http://lionsbrusquebercofiacao.org.br/wp-content/uploads/2015/12/logo-leoes.png"
+
+def HISTORIA():
+    MONTANHA
