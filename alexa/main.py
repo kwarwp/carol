66980b81cd8070695798b45f4108d945a229099e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:19:16 -0300

    Saved alexa/main.py at Timestamp: 2018-10-05 14:19:16

--- alexa/main.py
+++ alexa/main.py
@@ -3,4 +3,8 @@ from _spy.vittolino.mail import Cena, Elemento, Texto
 
 MACACO ="http://imagensemoldes.com.br/wp-content/uploads/2018/03/Imagem-de-Desenhos-%E2%80%93-Imagem-Bob-Zoom-Macaco-1-PNG-236x300.png"
 def Historia():
-    MATA = Cena(img = "https://mundoeduca.bol.uol.com.br/upload/conteudo_legenda/203947a3b090fb4199854898fda07ae9.jpg") 
+    MATA = Cena(img = "https://mundoeduca.bol.uol.com.br/upload/conteudo_legenda/203947a3b090fb4199854898fda07ae9.jpg")
+    MATA.vai()
+Historia()    
+    
+    
