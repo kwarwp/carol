174df51a645eb494e14f8e98989f2fd88bdc0589
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:51:19 -0300

    Saved amanda/main.py at Timestamp: 2018-10-05 14:51:19

--- amanda/main.py
+++ amanda/main.py
@@ -4,6 +4,6 @@ from _spy.vitollino.main import Cena,Elemento,Texto
 HOMEM = "https://1.bp.blogspot.com/-RilnuddcuWQ/Wc2BC-eYG8I/AAAAAAAABGs/tLwWjcNOig4gxl7RGF6lN-uPKrLVoKPqQCLcBGAs/s320/arvore_que_abraca%2B2.png"
 def Historia():
     ARVORES = Cena(img = "https://pensamentoverde.com.br/wp-content/uploads/2016/11/1-arvore.jpg")
-    HOMEM = Elemento
+    homem = Elemento(img=HOMEM, tit="Homem", style=dict(left=150, top=60, width=60, height=200))
     ARVORES.vai()
 Historia()
