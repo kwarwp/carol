44ba4598b0e7286cbde41ce598a49fdcbcc45c33
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:23:50 -0300

    Saved anastasia/main.py at Timestamp: 2018-10-05 14:23:49

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,8 +1,10 @@
 #Cibele
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 
 SAPO ="https://www.imagenspng.com.br/wp-content/uploads/2015/04/galinha-pintadinha-sapo-1.png"
 
 def Historia():
     CASTELO = Cena(img = "http://obviousmag.org/grifo/assets_c/2016/05/castelo-thumb-800x400-149789.jpg")
+    CASTELO.vai()
+Historia()
     
