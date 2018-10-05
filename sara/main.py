a1bad3bb1408893c230914948d1743b110fba266
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 10:57:10 -0300

    Saved sara/main.py at Timestamp: 2018-10-05 13:57:10

--- sara/main.py
+++ sara/main.py
@@ -1,4 +1,6 @@
 #maibi
 from _spy.vittolino.main import Cena, Elemento, texto
-hospital ="http://osshmtj.org.br/layout/imagens/h02.jpg"
-médico ="http://www.policlinicanovasaude.com.br/wp-content/uploads/2016/07/voce-medico.png"
+Hospital ="http://osshmtj.org.br/layout/imagens/h02.jpg"M
+Médico ="http://www.policlinicanovasaude.com.br/wp-content/uploads/2016/07/voce-medico.png"
+
+def Historia():
