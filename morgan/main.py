54dae8d3f28ee026313d85e423bda7cb55d8c4a2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:54:04 -0300

    Saved morgan/main.py at Timestamp: 2018-10-05 14:54:04

--- morgan/main.py
+++ morgan/main.py
@@ -5,7 +5,7 @@ ZUMBI ="http://www.deezer-blog.com/assets/http://www.deezer-blog.com/assets/site
 
 def Historia():
     ILHA = Cena(img ="https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/x06_10_2017.-Jornal-O-Globo.-Ilha-Grande-4.jpg.pagespeed.ic.NPxcsQST9i.jpg")
-    ZUMBI= Elemento
+    zumbi9= Elemento(img=ZUMBI,tit="ZUMBI",style=width(let=''150,top =60,heigit=200)
     ILHA.Vai ()
 Historia()
  
