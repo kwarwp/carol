92dccd6da0171d42ea5974f063b013d456317471
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:31:02 -0300

    Saved soraya/__log__.py at Timestamp: 2018-10-05 14:31:02

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -183,3 +183,21 @@ Traceback (most recent call last):
   module <module> line 2
     from _spy.vitollino.main import Cena, Elemento, texto
 '''},
+{'date': 'Fri Oct 05 2018 11:31:02.805 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    Historia()
+  module <module> line 7
+    DESERTO = CENA(img = "https://c1.staticflickr.com/4/3096/2913346696_9e9d1b3fa4.jpg")
+NameError: name 'CENA' is not defined
+'''},
