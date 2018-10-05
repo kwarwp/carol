cd54583c27b995102ea1b845d6af34c9111beede
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:30:10 -0300

    Saved adda/main.py at Timestamp: 2018-10-05 14:30:09

--- adda/main.py
+++ adda/main.py
@@ -3,7 +3,7 @@ from _ spy.vitollino.main import Cena, Elemento, Texto
 
 SIRI ="https://i.pinimg.com/originals/ff/00/51/ff005199bfa12a152819f86b196a6037.png"
 
-def historia():
+def Historia():
     PRAIA = Cena(img = "https://www.fundospaisagens.com/Imagens/fundo-de-ecra-de-praia.jpg")
     PRAIA.vai()
-HISTORIA()
+Historia()
