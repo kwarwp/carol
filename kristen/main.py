a4a0a070bc2be976087d293db3c968290fc818c1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:02:14 -0300

    Saved kristen/main.py at Timestamp: 2018-10-05 14:02:13

--- kristen/main.py
+++ kristen/main.py
@@ -2,3 +2,50 @@
 from _spy.vittolino.main import Cena, Elemento, Texto
 DESERTO ="https://cdn.pixabay.com/photo/2016/09/08/13/58/desert-1654439_960_720.jpg"
 CAMELO ="https://cdn.pixabay.com/photo/2017/01/17/18/45/camel-1987672_960_720.png"
+
+def Historia():
+    DESERTO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
