61216435f14166dbe95d28d8c251bd6132b8cbdf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:19:05 -0300

    Saved naomi/main.py at Timestamp: 2018-10-05 14:19:04

--- naomi/main.py
+++ naomi/main.py
@@ -3,7 +3,7 @@ from _spy.vittolino.main import Cena, Elemento, Texto
 
 LEAO = "http://lionsbrusquebercofiacao.org.br/wp-content/uploads/2015/12/logo-leoes.png"
 
-def HISTORIA():
+def Historia():
     MONTANHA = cena(img = "https://www.fundoswiki.com/Uploads/fundoswiki.com/ImagensGrandes/paisagem-montanha-9.jpg") 
-    CASTELO.VAI()
-historia()
+    MONTANHA.VAI()
+Historia()
