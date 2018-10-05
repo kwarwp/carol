e6dafd14c4b2abf7271f00f3d1d6177b2560ca39
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:02:12 -0300

    Saved morgan/main.py at Timestamp: 2018-10-05 14:02:11

--- morgan/main.py
+++ morgan/main.py
@@ -2,3 +2,6 @@
 from_spy.vittolino.main import cena,Elemento,texto
 ILHA = "https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/06_10_2017.-Jornal-O-Globo.-Ilha-Gra
 ZUMBI ="http://www.deezer-blog.com/assets/http://www.deezer-blog.com/assets/sites/8/2015/10/zumbiblog.pngsites/8/2015/10/zumbiblog.png"
+
+def historia():
+    ILHA
