0c6595b7aea02418b04f69c9b2a0538fdc885177
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 10:57:36 -0300

    Saved morgan/main.py at Timestamp: 2018-10-05 13:57:36

--- morgan/main.py
+++ morgan/main.py
@@ -1,3 +1,4 @@
 # daiane
 from_spy.vittolino.main import cena,Elemento,texto
-ILHA = "https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/06_10_2017.-Jornal-O-Globo.-Ilha-Grande-4.jpg"
+ILHA = "https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/06_10_2017.-Jornal-O-Globo.-Ilha-Gra
+ZUMBI ="http://www.deezer-blog.com/assets/http://www.deezer-blog.com/assets/sites/8/2015/10/zumbiblog.pngsites/8/2015/10/zumbiblog.png"
