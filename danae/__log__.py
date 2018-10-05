5cd556e2ccfdbc9b2aa0f687e173d0bc4bf13430
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:19:38 -0300

    Created danae/__log__.py at Timestamp: 2018-10-05 14:19:37

---
+++ danae/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Oct 05 2018 11:19:35.625 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  CASTELO = Cena(img = "http://obviousmag.org/grifo/assets_c/2016/05/castelo-thumb-800x400-149789.jpg")]
+                                                                                                       ^
+SyntaxError: invalid syntax
+'''},
