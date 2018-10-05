7314e0588f1a8c7c3017c5cf089bebb1579189de
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:25:51 -0300

    Saved soraya/__log__.py at Timestamp: 2018-10-05 14:25:51

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -35,3 +35,7 @@ Exception: <SyntaxError: Unexpected token {>
 'error': '''
 Exception: <SyntaxError: Unexpected token {>
 '''},
+{'date': 'Fri Oct 05 2018 11:25:52.804 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: <SyntaxError: Unexpected token {>
+'''},
