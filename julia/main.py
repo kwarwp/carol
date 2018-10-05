50f0a2270163e4cf779eab287268d1b89eab4e61
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 10:58:04 -0300

    Saved julia/main.py at Timestamp: 2018-10-05 13:58:04

--- julia/main.py
+++ julia/main.py
@@ -2,3 +2,5 @@
 from _spy.vittolino.main import Cena, Elemento,Texto
 MOTELMEDIEVAL ="http://www.motel-medieval.com.br/conteudo/wp-content/uploads/15-4-640x440.jpg"
 AMANTES ETERNOS ="https://rodrigobrum.files.wordpress.com/2015/01/amantes-eternos.png"
+
+def Historia():
