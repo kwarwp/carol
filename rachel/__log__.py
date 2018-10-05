c35f9b07d9480c9b9895768f80f9a6e0966b64d2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:20:50 -0300

    Saved rachel/__log__.py at Timestamp: 2018-10-05 14:20:50

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -17,3 +17,21 @@
     CASADEBONECA = Cena (img = "https://img01-olxpt.akamaized.net/img-olxpt/930449724_1_644x461_casa-de-bonecas-em-madeira-macia-novo-porto_rev001.jpg")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Fri Oct 05 2018 11:20:51.385 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    Historia()    
+  module <module> line 6
+    CASADEBONECA = Cena (img = "https://img01-olxpt.akamaized.net/img-olxpt/930449724_1_644x461_casa-de-bonecas-em-madeira-macia-novo-porto_rev001.jpg")
+TypeError: 'module' object is not callable
+'''},
