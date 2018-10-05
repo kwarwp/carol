880913841710f1e664cadb38081729132f4a486a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:52:35 -0300

    Saved tracy/main.py at Timestamp: 2018-10-05 14:52:35

--- tracy/main.py
+++ tracy/main.py
@@ -5,6 +5,6 @@ ANDORINHAS = "https://meiosdigitais2014.files.wordpress.com/2014/05/passaros.png
 
 def Historia():
     PORDOSOL = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/c/ce/P%C3%B4r_do_Sol.jpg")
-    ANDORINHAS = Elemento
+    andorinhas = Elemento(img=ANDORINHAS, tit="Andorinhas", style=dict(left=150, top=60, width=60, height=200))
     PORDOSOL.vai()
 Historia()
