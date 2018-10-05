69d66811ff61c30e3dcfcc850e60468fa1142805
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:24:26 -0300

    Saved danae/main.py at Timestamp: 2018-10-05 14:24:25

--- danae/main.py
+++ danae/main.py
@@ -1,5 +1,9 @@
 # regina cornelio
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
+
 SAPO ="https://www.imagenspng.com.br/wp-content/uploads/2015/04/galinha-pintadinha-sapo-1.png"
+
 def Historia():
     CASTELO = Cena(img = "http://obviousmag.org/grifo/assets_c/2016/05/castelo-thumb-800x400-149789.jpg")
+    CASTELO.vai()
+Historia()
