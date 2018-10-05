504a7a4865a4fe6917a71ba4b856ae446026d117
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:02:02 -0300

    Saved meredith/main.py at Timestamp: 2018-10-05 14:02:01

--- meredith/main.py
+++ meredith/main.py
@@ -2,3 +2,6 @@
 from _ spy.vittolino.main import Cena, Elemento,Texto
 PRAIA ="http://www.pousadazemaria.com.br/system/images/pousada-ze-maria-fernando-de-noronha-t237.jpg"
 GOLFINHO ="https://png2.kisspng.com/20180213/dqw/kisspng-dolphin-mahi-mahi-seawater-fish-aquarium-dolphin-5a83bd27e0a2b4.2220066615185830799201.png"
+
+def HISTORIA():
+    PRAIA
