61586419fbb2ea16429b21613101e6dbc5bcfc6d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:30:18 -0300

    Saved soraya/main.py at Timestamp: 2018-10-05 14:30:18

--- soraya/main.py
+++ soraya/main.py
@@ -1,5 +1,5 @@
-# carol.soraya.main.py
-from_spy.vitollino.main import Cena, Elemento, texto
+#matheusterra
+from _spy.vitollino.main import Cena, Elemento, texto
 
 SUNAGAURE = "https://c1.staticflickr.com/4/3096/2913346696_9e9d1b3fa4.jpg"
 
