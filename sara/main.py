343cfccbdb8cb0716a6dbfcf3de4c42962b41aec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 10:26:27 -0300

    Saved sara/main.py at Timestamp: 2018-10-05 13:26:25

--- sara/main.py
+++ sara/main.py
@@ -1,4 +1,3 @@
-#lara 
-from _spy.vittoline.mainimport Cena,Elemento,Texto
-CASTELO ="https://http2.mlstatic.com/painel-sublimado-castelo-da-cinderela-20-x-30-sem-emenda-D_NQ_NP_881065-MLB25608613215_052017-F.jpg"
-PERSONAGEM ="http://soseriadosdetv.com/wp-content/uploads/2017/08/dpa-detetives-do-predio-azul-9-temporada.png"
+#maibi
+from _spy.vittolino.main import Cena, Elemento, texto
+hospital ="http://osshmtj.org.br/layout/imagens/h02.jpg"
