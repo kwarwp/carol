fbb5587082003b44e96b5303abde518961350bed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 10:46:49 -0300

    Saved amanda/main.py at Timestamp: 2018-10-05 13:46:49

--- amanda/main.py
+++ amanda/main.py
@@ -1,3 +1,4 @@
 # luana cristina
 from _spy.vittolino.main import Cena,Elemento,Texto
 ARVORES = "https://pensamentoverde.com.br/wp-content/uploads/2016/11/1-arvore.jpg"
+HOMEM = "https://1.bp.blogspot.com/-RilnuddcuWQ/Wc2BC-eYG8I/AAAAAAAABGs/tLwWjcNOig4gxl7RGF6lN-uPKrLVoKPqQCLcBGAs/s320/arvore_que_abraca%2B2.png"
