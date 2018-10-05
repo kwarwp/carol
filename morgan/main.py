1b2b2d6976f166200800217912431e36cb1be518
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:43:59 -0300

    Saved morgan/main.py at Timestamp: 2018-10-05 14:43:59

--- morgan/main.py
+++ morgan/main.py
@@ -7,4 +7,4 @@ def Historia():
     ILHA = Cena(img ="https://ogimg.infoglobo.com.br/in/21993934-fe0-499/FT1086A/652/x06_10_2017.-Jornal-O-Globo.-Ilha-Grande-4.jpg.pagespeed.ic.NPxcsQST9i.jpg")
     ILHA.Vai ()
 Historia()
-    
+ ZUMBI=elemento
