85306c90d888efdb20a46a6d21902b2d531eb546
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:20:00 -0300

    Saved kristen/main.py at Timestamp: 2018-10-05 14:20:00

--- kristen/main.py
+++ kristen/main.py
@@ -1,10 +1,12 @@
 #Jerry
 from _spy.vittolino.main import Cena, Elemento, Texto
+
 CAMELO ="https://cdn.pixabay.com/photo/2017/01/17/18/45/camel-1987672_960_720.png"
 
 def Historia():
     DESERTO = Cena(img = "https://cdn.pixabay.com/photo/2016/09/08/13/58/desert-1654439_960_720.jpg")
-    
+    DESERTO.vai()
+Historia()    
 
 
 
