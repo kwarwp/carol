a7f7e7ab4c87ebd02fb981fe93b7f26bff2d6fae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:13:25 -0300

    Saved rachel/main.py at Timestamp: 2018-10-05 14:13:25

--- rachel/main.py
+++ rachel/main.py
@@ -1,4 +1,7 @@
 # maria
 from _spy.vittolino.main import Cena, Elemento, Texto
-CASADEBONECA ="https://img01-olxpt.akamaized.net/img-olxpt/930449724_1_644x461_casa-de-bonecas-em-madeira-macia-novo-porto_rev001.jpg"
+
 BONECA ="https://images-americanas.b2w.io/produtos/01/00/item/128261/5/128261584_1GG.jpg"
+def Historia() :
+    CASADEBONECA = Cena (img = "https://img01-olxpt.akamaized.net/img-olxpt/930449724_1_644x461_casa-de-bonecas-em-madeira-macia-novo-porto_rev001.jpg")
+    
