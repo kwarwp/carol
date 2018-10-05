d6e9383c8a591382e807b8d8133ab0165b9e833a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:23:50 -0300

    Saved meredith/main.py at Timestamp: 2018-10-05 14:23:50

--- meredith/main.py
+++ meredith/main.py
@@ -1,7 +1,10 @@
 # juliana
-from _ spy.vittolino.main import Cena, Elemento,Texto
+from _ spy.vitollino.main import Cena, Elemento,Texto
 
 GOLFINHO ="https://png2.kisspng.com/20180213/dqw/kisspng-dolphin-mahi-mahi-seawater-fish-aquarium-dolphin-5a83bd27e0a2b4.2220066615185830799201.png"
 
-def HISTORIA():
+def Historia():
     PRAIA = Cena(img = "http://www.pousadazemaria.com.br/system/images/pousada-ze-maria-fernando-de-noronha-t237.jpg")
+    PRAIA.vai()
+Historia()
+    
