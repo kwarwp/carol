98d375d0cceb91cd8d65c3d9e2a6c80f0d5ba436
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-05 11:02:16 -0300

    Saved anastasia/main.py at Timestamp: 2018-10-05 14:02:15

--- anastasia/main.py
+++ anastasia/main.py
@@ -2,3 +2,6 @@
 from _spy.vittolino.main import Cena, Elemento, Texto
 CASTELO ="http://obviousmag.org/grifo/assets_c/2016/05/castelo-thumb-800x400-149789.jpg"
 SAPO ="https://www.imagenspng.com.br/wp-content/uploads/2015/04/galinha-pintadinha-sapo-1.png"
+
+def Historia():
+    CASTELO
