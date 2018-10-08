ccdb387adaa66532dae51ac36ec51bcaf4dcaf91
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:12:17 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-08 21:12:16

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,8 +1,8 @@
 #Cibele
-from _spy.vittolino.main import Cena, Elemento, Texto
-CAMPO DE FUTEBOL ="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/The_deep_blue_sea_%286834127561%29.jpg/800px-The_deep_blue_sea_%286834127561%29.jpg"
-MBAAP ="https://dj0j0ofql4htg.cloudfront.net/cms2/image_manager/uploads/News/285166/7/default.jpg"
+from _spy.vitollino.main import Cena, Elemento, Texto 
+
+girl="https://www.google.com.br/search?safe=strict&rlz=1C1SQJL_pt-BRBR818&biw=1366&bih=657&tbm=isch&sa=1&ei=A8G7W4SND5KQ_QbYzYeYDQ&q=anime+kawaii+neko&oq=anime+kaw&gs_l=img.1.6.0l4j0i67k1l2j0l4.147118.151008.0.153273.17.13.0.1.1.0.265.1698.0j7j2.9.0....0...1c.1.64.img..8.9.1458....0.TQfqXOdsz4s"
 def Historia():
-    cenaCampodefutebol = Cena(img = "http://cidadesnanet.com/news/wp-content/uploads/2015/05/IMG-20150429-WA0081-1900x900_c.jpg")                                                                                                               
-     cenaCampodefutebol.vai
-Historia()                                                                                                                                                                                                    
+        CenaBeach = cena(img = "https://www.google.com.br/search?q=praia&safe=strict&rlz=1C1SQJL_pt-BRBR818&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiVv9Hu2ffdAhUC1lkKHU6VBw4Q_AUIDigB&biw=1366&bih=657#imgrc=ztCLFkXnxXHEUM:")
+        CenaBeach.vai()
+Historia()
