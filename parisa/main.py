ef3fe43e4527b4ab99cea54b8ca156ccb064ed82
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:02:28 -0300

    Saved parisa/main.py at Timestamp: 2018-10-08 21:02:27

--- parisa/main.py
+++ parisa/main.py
@@ -3,5 +3,6 @@ from _spy.vittolino.main import Cena, Elemento,Texto
 FLORESTA="http://homeroreis.com/wp-content/uploads/2015/10/floresta.jpg"
 CAÇADOR="http://www.culturamix.com/wp-content/gallery/cacador-2/images.png"
 def Historia():
-      cenaFloresta = Cena(ing = "")
+    cenaFloresta = Cena(ing = "http://homeroreis.com/wp-content/uploads/2015/10/floresta.jpg")
+    cenaFloresta.vai()
 Historia()
