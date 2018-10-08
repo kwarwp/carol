6f9ab604078ac6008a9efe655ed5d0ae680718ea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 17:52:29 -0300

    Saved roxanne/main.py at Timestamp: 2018-10-08 20:52:29

--- roxanne/main.py
+++ roxanne/main.py
@@ -3,5 +3,5 @@ from _spy.vittolino.main import Cena,Elemento,Texto
 PARQUE ="https://cdn.civitatis.com/argentina/buenos-aires/guia/parque-tres-febrero-grid-m.jpg"
 NINJA ="http://pngimg.com/uploads/ninja/ninja_PNG8.png"
 def Historia():
-    cenaParque
+    cenaParque = Cena(img = "https://cdn.civitatis.com/argentina/buenos-aires/guia/parque-tres-febrero-grid-m.jpg")
 Historia()
