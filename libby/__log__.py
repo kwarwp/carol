78d8b0599610d4686da98bc0a1747d8886ffb8bd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:04:01 -0300

    Created libby/__log__.py at Timestamp: 2018-10-08 21:04:00

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 08 2018 18:03:58.16 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from _spy.vittoline.mainimport Cena,Elemento,Texto
+                                     ^
+SyntaxError: invalid syntax
+'''},
