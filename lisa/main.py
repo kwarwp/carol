9510fbbcbddf9945f531c4a1468636647e71a289
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:03:51 -0300

    Saved lisa/main.py at Timestamp: 2018-10-08 21:03:50

--- lisa/main.py
+++ lisa/main.py
@@ -1,6 +1,6 @@
 #Lucas
 from _spy.vittolino.Main import Cena, Elemento, Texto
-TEATRO ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg"
+"
 GOKU ="http://rescuedesk.me/wp-content/uploads/super-saiyan-2-goku-aura-by-brusselthesaiyan-daemjz9-png.png"
 def Historia():
     cenaTeatro = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
