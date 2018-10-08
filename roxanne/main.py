68a3123fbfb75e2299a55491ef1c73095c2140bd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 17:46:51 -0300

    Saved roxanne/main.py at Timestamp: 2018-10-08 20:46:50

--- roxanne/main.py
+++ roxanne/main.py
@@ -2,3 +2,6 @@
 from _spy.vittolino.main import Cena,Elemento,Texto
 PARQUE ="https://cdn.civitatis.com/argentina/buenos-aires/guia/parque-tres-febrero-grid-m.jpg"
 NINJA ="http://pngimg.com/uploads/ninja/ninja_PNG8.png"
+def Historia():
+    cenaParque
+Historia()
