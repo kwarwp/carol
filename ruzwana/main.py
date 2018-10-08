617fa1c4d9e45c63573277fdf72c21c790f0eccd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 17:43:28 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-08 20:43:27

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -2,3 +2,6 @@
 from _spy.vittolino.main import Cena, Elemento, Texto
 SEA ="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/The_deep_blue_sea_%286834127561%29.jpg/800px-The_deep_blue_sea_%286834127561%29.jpg"
 FISH ="https://www.freeiconspng.com/uploads/fish-png-transparent-15.png"
+def Historia():
+
+Historia()
