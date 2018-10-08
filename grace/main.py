edadfc76d1a2f2c972f64e790f072feb926264a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:19:03 -0300

    Saved grace/main.py at Timestamp: 2018-10-08 21:19:03

--- grace/main.py
+++ grace/main.py
@@ -1,8 +1,8 @@
 #GabrielLucas e Brenno
 from _spy.vittolino.main import Cena, Elemento, Texto
 
-MARIOBROS = "https://ranktopten.com/upload/pf/z_/mario_400x400.png"
-def HISTORIA():
-    cenaCastle = Cena(img="http://www.ibefpr.com.br/images/stories/easygallery/resized/56/1260469907_castelo%20do%20batel.jpg")
+MarioBros= "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj9jJKM2ffdAhWMjFkKHZo8ApYQjRx6BAgBEAU&url=https%3A%2F%2Franktopten.com%2Ftop-super-smash-flash-2-characters-4665%2Fmario&psig=AOvVaw3YFWeWIA0rVCDDygbNlK1G&ust=1539116971073924"
+def Historia():
+    cenaCastle=Cena(img = "https://http2.mlstatic.com/fundo-fotografico-tecido-newborn-3d-castelo-150m-x-220m-D_NQ_NP_812472-MLB27642632585_062018-O.webp")
     cenaCastle.vai()
-HISTORIA()
+Historia()
