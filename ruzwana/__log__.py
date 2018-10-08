e66247f9f1a441eb8bd9b1bf1f3517834e25a481
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:12:37 -0300

    Saved ruzwana/__log__.py at Timestamp: 2018-10-08 21:12:37

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -71,3 +71,21 @@ NameError: name 'cena' is not defined
     CenaBeach = cena(img = "https://www.google.com.br/search?q=praia&safe=strict&rlz=1C1SQJL_pt-BRBR818&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiVv9Hu2ffdAhUC1lkKHU6VBw4Q_AUIDigB&biw=1366&bih=657#imgrc=ztCLFkXnxXHEUM:")
 NameError: name 'cena' is not defined
 '''},
+{'date': 'Mon Oct 08 2018 18:12:36.582 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    Historia()
+  module <module> line 6
+    CenaBeach = cena(img = "https://www.google.com.br/search?q=praia&safe=strict&rlz=1C1SQJL_pt-BRBR818&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiVv9Hu2ffdAhUC1lkKHU6VBw4Q_AUIDigB&biw=1366&bih=657#imgrc=ztCLFkXnxXHEUM:")
+NameError: name 'cena' is not defined
+'''},
