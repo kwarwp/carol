47fb43536e37150b343722bbfda75b29387cba32
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:02:26 -0300

    Saved stacy/main.py at Timestamp: 2018-10-08 21:02:26

--- stacy/main.py
+++ stacy/main.py
@@ -4,5 +4,5 @@ DISNEY ="https://www.vaipradisney.com/blog/wp-content/uploads/2017/05/QUARTO-CAS
 CINDERELA="http://imagensemoldes.com.br/wp-content/uploads/2018/04/Imagem-de-Personagens-Princesa-Cinderela-10-PNG-244x300.png"
 def HistoriA():
     cenaCastelo = Cena(img ="https://www.vaipradisney.com/blog/wp-content/uploads/2017/05/QUARTO-CASTELO-CINDERELA-13.jpg")
-    
+    cena cstelo.vai()    
 Historia()
