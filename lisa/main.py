9b00281ea7615e512b622fb648697a1a69e9a196
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:02:22 -0300

    Saved lisa/main.py at Timestamp: 2018-10-08 21:02:22

--- lisa/main.py
+++ lisa/main.py
@@ -4,4 +4,5 @@ TEATRO ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8X
 GOKU ="http://rescuedesk.me/wp-content/uploads/super-saiyan-2-goku-aura-by-brusselthesaiyan-daemjz9-png.png"
 def Historia():
     cenaTeatro = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
+    cenaTeatro.vai()
 Historia()
