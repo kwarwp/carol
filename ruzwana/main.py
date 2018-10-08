e641dc404ab3e5742aa9bc3af6ffb785b72db65d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:03:46 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-08 21:03:46

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -4,4 +4,5 @@ CAMPO DE FUTEBOL ="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/The
 MBAAP ="https://dj0j0ofql4htg.cloudfront.net/cms2/image_manager/uploads/News/285166/7/default.jpg"
 def Historia():
     cenaCampodefutebol = Cena(img = "http://cidadesnanet.com/news/wp-content/uploads/2015/05/IMG-20150429-WA0081-1900x900_c.jpg")                                                                                                               
+     cenaCampodefutebol.vai
 Historia()                                                                                                                                                                                                    
