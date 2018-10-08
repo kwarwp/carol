886edbdc086e536e2fdb9b0499151a1f5d38f3ec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:25:12 -0300

    Saved parisa/main.py at Timestamp: 2018-10-08 21:25:12

--- parisa/main.py
+++ parisa/main.py
@@ -1,8 +1,8 @@
 #Arthur Correa  
 from _spy.vitollino.main import Cena, Elemento,Texto
-FLORESTA="http://homeroreis.com/wp-content/uploads/2015/10/floresta.jpg"
+
 CACADOR="http://www.culturamix.com/wp-content/gallery/cacador-2/images.png"
-def Hitoria():
-    cenaFloresta=Cena(img="http://homeroreis.com/wp-content/uploads/2015/10/floresta.jpg"
+def Historia():
+    cenaFloresta=Cena(img="http://homeroreis.com/wp-content/uploads/2015/10/floresta.jpg")
     cenaFloresta.vai()
-Histaria()
+Historia()
