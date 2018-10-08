9b6246156c58c11d64b6aac0de81bc9661208100
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:26:03 -0300

    Saved callie/main.py at Timestamp: 2018-10-08 21:26:02

--- callie/main.py
+++ callie/main.py
@@ -2,3 +2,6 @@
 from _spy.vittolino.main import Cena, Elemento, Texto
 IMGOD ="http://3.bp.blogspot.com/-PSaNS37veEI/UqkOmMBXcvI/AAAAAAAAAIQ/Nqv0XHO5fKk/s1600/Sonic.exe4.jpg"
 SONICEXE ="http://pixelartmaker.com/art/5c608d29642e09e.png"
+
+def historia():
+    cena
