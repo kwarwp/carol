4f4a730084311956cbbf8d0d5381c85ebacf60cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:23:00 -0300

    Saved kathryn/main.py at Timestamp: 2018-10-08 21:22:59

--- kathryn/main.py
+++ kathryn/main.py
@@ -3,6 +3,6 @@ from _spy.vitollino.main import Cena,Elemento,Texto
 
 PAWERRANGER="https://1.bp.blogspot.com/-mCl9tarfYd4/V5z6bLvJVWI/AAAAAAAAIAo/pgYvjjqTqn8oMXxLU_NQWyXQkIBg2kFkQCLcB/s1600/mighty_morphin__power_rangers__by_camo_flauge-da1l9zn.png"
 def Historia():
-    cenaPraia = Cena (ing = "https://ogimg.infoglobo.com.br/boa-viagem/23123046-813-45f/GEOMIDIA/375/xwater-3092908_1920.jpg.pagespeed.ic.7q4TE_WU0u.jpg")
-    cenaPraia.vai()
+    cenaFloresta = Cena (img = "https://ogimg.infoglobo.com.br/boa-viagem/23123046-813-45f/GEOMIDIA/375/xwater-3092908_1920.jpg.pagespeed.ic.7q4TE_WU0u.jpg")
+    cenaFloresta.vai()
 Historia()
