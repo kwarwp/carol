1ad185e72694ccec802f3993658ccca66fb575a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 17:33:00 -0300

    Saved courtney/main.py at Timestamp: 2018-10-08 20:32:58

--- courtney/main.py
+++ courtney/main.py
@@ -2,6 +2,4 @@
 # yasmin.
 from _spy.vittolino.main import Cena,Elemento,Texto
 CASTELO ="https://ae01.alicdn.com/kf/HTB17VXqRVXXXXbGXpXXq6xXFXXX8/2017-3D-Yeni-5D-Elmas-Nak-Manzara-K-Kar-Kale-Bah-e-Evi-mazayka-Elmas-Mozaik.jpg_640x640.jpg""
-PERSONAGEM"https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjBtfnOm-vdAhVIPJAKHVvMAKEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D80czcfCpq9Y&psig=AOvVaw13g6nCRnfdwWPBulkirMUE&ust=1538688343731260"
-
-
+PERSONAGEM"https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjmwdSo2PfdAhVtuVkKHdWDAMoQjRx6BAgBEAU&url=http%3A%2F%2Flife-writer.tumblr.com%2Fpost%2F123761609710%2Fi-missed-my-kids&psig=AOvVaw1kWh3XpDoI0P2G30m_UH6o&ust=1539116915250785"
