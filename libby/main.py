554b75243e0ccf78417787f2a1ac1f65350e069d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 17:44:23 -0300

    Saved libby/main.py at Timestamp: 2018-10-08 20:44:23

--- libby/main.py
+++ libby/main.py
@@ -1,4 +1,6 @@
 #laura
 from _spy.vittoline.mainimport Cena,Elemento,Texto
 CASTELO ="http://i67.tinypic.com/f24z7l.jpg"
-bunequinho =""
+bunequinho ="http://56.media.tumblr.com/b29d0f586c54e40d71c8566efba0cecd/tumblr_o31t6fLBbo1tx4472o1_1280.png" 
+def Historia():
+
