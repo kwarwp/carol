605bb375f74d2a2363e861bbb374d0818b1f2509
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:04:04 -0300

    Created stacy/__log__.py at Timestamp: 2018-10-08 21:04:04

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 08 2018 18:04:00.462 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  cena cstelo.vai()    
+        ^
+SyntaxError: invalid syntax
+'''},
