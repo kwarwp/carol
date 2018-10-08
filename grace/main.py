28b62eb4e2178fce56bb3b0ad3e0ce5a8a08e057
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 17:44:25 -0300

    Saved grace/main.py at Timestamp: 2018-10-08 20:44:25

--- grace/main.py
+++ grace/main.py
@@ -1,4 +1,6 @@
 #GabrielLucas e Brenno
 from _spy.vittolino.main import Cena, Elemento, Texto
 CASTLE ="http://www.ibefpr.com.br/images/stories/easygallery/resized/56/1260469907_castelo%20do%20batel.jpg"
-MARIO BROS =https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwipsdCj1vfdAhUNjq0KHSHwCHcQjRx6BAgBEAU&url=http%3A%2F%2Fparody.wikia.com%2Fwiki%2FFile%3AMario-Bros.png&psig=AOvVaw2YPWLg4zNogZNuS51aYzxZ&ust=1539116590938402
+MARIO BOSS https://ranktopten.com/upload/pf/z_/mario_400x400.png
+def HISTORIA():
+HISTORIA()
