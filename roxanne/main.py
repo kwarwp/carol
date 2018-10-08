2921875b333b13b02c380415987e8de4dc4b71b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 17:31:54 -0300

    Saved roxanne/main.py at Timestamp: 2018-10-08 20:31:54

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,4 +1,4 @@
 #davy
 from _spy.vittolino.main import Cena,Elemento,Texto
-PARQUE ="https://cdn.civitatis.com/italia/milan/guia/parque-sempione.jpg"
-NINJA =""
+PARQUE ="https://cdn.civitatis.com/argentina/buenos-aires/guia/parque-tres-febrero-grid-m.jpg"
+NINJA ="http://pngimg.com/uploads/ninja/ninja_PNG8.png"
