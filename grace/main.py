aea72154981761c67725c5620bf8878bd46e67de
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:03:48 -0300

    Saved grace/main.py at Timestamp: 2018-10-08 21:03:48

--- grace/main.py
+++ grace/main.py
@@ -1,6 +1,6 @@
 #GabrielLucas e Brenno
 from _spy.vittolino.main import Cena, Elemento, Texto
-CASTLE ="http://www.ibefpr.com.br/images/stories/easygallery/resized/56/1260469907_castelo%20do%20batel.jpg"
+
 MARIOBROS = "https://ranktopten.com/upload/pf/z_/mario_400x400.png"
 def HISTORIA():
     cenaCastle = Cena(img="http://www.ibefpr.com.br/images/stories/easygallery/resized/56/1260469907_castelo%20do%20batel.jpg")
