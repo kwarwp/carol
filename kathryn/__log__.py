4a77f621f8707caa8fb3dcdc33f96949e0ee0a80
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:04:17 -0300

    Created kathryn/__log__.py at Timestamp: 2018-10-08 21:04:17

---
+++ kathryn/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Mon Oct 08 2018 18:04:15.173 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    Historia()
+  module <module> line 6
+    cenaPraia = Cena (ing = "https://ogimg.infoglobo.com.br/boa-viagem/23123046-813-45f/GEOMIDIA/375/xwater-3092908_1920.jpg.pagespeed.ic.7q4TE_WU0u.jpg")
+TypeError: 'module' object is not callable
+'''},
