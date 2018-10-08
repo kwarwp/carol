f0aedf5604c62c7f46192912a7e2ededbb5550a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:03:37 -0300

    Saved stacy/main.py at Timestamp: 2018-10-08 21:03:37

--- stacy/main.py
+++ stacy/main.py
@@ -1,6 +1,6 @@
 # gabriela
 from _spy.vittolino.main import Cena,Elemento,Texto
-DISNEY ="https://www.vaipradisney.com/blog/wp-content/uploads/2017/05/QUARTO-CASTELO-CINDERELA-13.jpg"
+
 CINDERELA="http://imagensemoldes.com.br/wp-content/uploads/2018/04/Imagem-de-Personagens-Princesa-Cinderela-10-PNG-244x300.png"
 def HistoriA():
     cenaCastelo = Cena(img ="https://www.vaipradisney.com/blog/wp-content/uploads/2017/05/QUARTO-CASTELO-CINDERELA-13.jpg")
