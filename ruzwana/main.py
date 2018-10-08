5f4a085693b9e976d732ea59a6670193ccbbdf04
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 17:56:51 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-08 20:56:50

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,7 +1,7 @@
 #Cibele
 from _spy.vittolino.main import Cena, Elemento, Texto
-SEA ="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/The_deep_blue_sea_%286834127561%29.jpg/800px-The_deep_blue_sea_%286834127561%29.jpg"
-FISH ="https://www.freeiconspng.com/uploads/fish-png-transparent-15.png"
+CAMPO DE FUTEBOL ="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/The_deep_blue_sea_%286834127561%29.jpg/800px-The_deep_blue_sea_%286834127561%29.jpg"
+MBAAP ="https://dj0j0ofql4htg.cloudfront.net/cms2/image_manager/uploads/News/285166/7/default.jpg"
 def Historia():
-
-Historia()
+    cenaCampodefutebol = Cena(img = "http://cidadesnanet.com/news/wp-content/uploads/2015/05/IMG-20150429-WA0081-1900x900_c.jpg")                                                                                                               
+Historia()                                                                                                                                                                                                    
