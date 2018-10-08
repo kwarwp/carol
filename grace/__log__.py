3a6ec597f0f12ccdffa42a7a1f2fa61138346c20
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-08 18:19:48 -0300

    Created grace/__log__.py at Timestamp: 2018-10-08 21:19:48

---
+++ grace/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Mon Oct 08 2018 18:19:44.248 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    Historia()
+  module <module> line 6
+    cenaCastle=Cena(img = "https://http2.mlstatic.com/fundo-fotografico-tecido-newborn-3d-castelo-150m-x-220m-D_NQ_NP_812472-MLB27642632585_062018-O.webp")
+TypeError: 'module' object is not callable
+'''},
