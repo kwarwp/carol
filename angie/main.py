5a8b92023b40d52f89129b27ab8757aa7661ff78
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:26:09 -0300

    Saved angie/main.py at Timestamp: 2018-10-17 21:26:09

--- angie/main.py
+++ angie/main.py
@@ -3,8 +3,8 @@ from _spy.vittolino.main import Cena,Elemento,Texto
 
 HAWAII ="https://cdn.pixabay.com/photo/2017/06/10/11/16/landscape-2389694_960_720.jpg"
 def historia():
-    cenaHawaii=cena(ing="https://www.bing.com/images/search?view=detailV2&ccid=QKsRkE7z&id=B360AF9CE9DE6C5E8D5F80197473AD7C76CF7DB5&thid=OIP.QKsRkE7z20HKB01_DmOTzgHaFc&mediaurl=https%3a%2f%2fc8.alamy.com%2fcomppt%2fcxr4n2%2fcaptadores-de-conduzir-atraves-de-uma-manga-grove-big-island-hawaii-estados-unidos-da-america-cxr4n2.jpg&exph=956&expw=1300&q=hawaii+endereco+de+imange&simid=608048392595177497&selectedIndex=3
-    anima=Elemento(ing=ANIMAL,
+cenaHawaii=Cena(ing="https://www.bing.com/images/search?view=detailV2&ccid=Y1T2pWEV&id=69E038ECD76C78D633FED0952F9EF3B9828180C8&thid=OIP.Y1T2pWEVQDV1V_NRIo78BAHaH3&mediaurl=https%3a%2f%2fst3.depositphotos.com%2f9237796%2f16585%2fi%2f1600%2fdepositphotos_165859096-stock-photo-tropical-hawaii-leaves-frame-in.jpg&exph=1700&expw=1600&q=wahaii+imagens+png&simid=608002487962437097&selectedIndex=24
+    anima=Elmento(ing=ANIMAL,
           tit="historias do mar",
           style = dict(left=150,top=60,width=60,height=200))
    animal.entra(cenaHawaii)
