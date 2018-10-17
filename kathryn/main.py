f4db22c46688b076dce73317ac4e1be684c8f8b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:10:18 -0300

    Saved kathryn/main.py at Timestamp: 2018-10-17 21:10:17

--- kathryn/main.py
+++ kathryn/main.py
@@ -6,6 +6,7 @@ def Historia():
     cenaFloresta = Cena (img = "https://ogimg.infoglobo.com.br/boa-viagem/23123046-813-45f/GEOMIDIA/375/xwater-3092908_1920.jpg.pagespeed.ic.7q4TE_WU0u.jpg")
     powearranger = Elemento(img=PAWER RENGER,
                    tit="Power renger",
-                   style=dict(left=150
+                   style=dict(left=150, top=50
+power ranger ,entra(cena floresta)               
     cenaFloresta.vai()
 Historia()
