ae66e873946e6d275fdee4b5c7403bb1b8168963
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:22:38 -0300

    Saved parisa/main.py at Timestamp: 2018-10-17 21:22:38

--- parisa/main.py
+++ parisa/main.py
@@ -1,7 +1,1 @@
 
-    cacador = Elemento(ing=CACADOR,
-              tit ="Caçador",
-              style = dict(left=150, top=60, width=60, height=200))
-    cacador.entra(cenaFloresta)
-    cenaFloresta.vai()
-Historia()
