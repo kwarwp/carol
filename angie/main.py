0ba7502e908bc3bdb58dc4ac726114189488cc41
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:08:42 -0300

    Saved angie/main.py at Timestamp: 2018-10-17 21:08:42

--- angie/main.py
+++ angie/main.py
@@ -1,3 +1,11 @@
 # bruna de sa
 from _spy.vittolino.main import Cena,Elemento,Texto
+
 HAWAII ="https://cdn.pixabay.com/photo/2017/06/10/11/16/landscape-2389694_960_720.jpg"
+def historia():
+    cenaHawaii=cena(ing="https://www.bing.com/images/search?view=detailV2&ccid=QKsRkE7z&id=B360AF9CE9DE6C5E8D5F80197473AD7C76CF7DB5&thid=OIP.QKsRkE7z20HKB01_DmOTzgHaFc&mediaurl=https%3a%2f%2fc8.alamy.com%2fcomppt%2fcxr4n2%2fcaptadores-de-conduzir-atraves-de-uma-manga-grove-big-island-hawaii-estados-unidos-da-america-cxr4n2.jpg&exph=956&expw=1300&q=hawaii+endereco+de+imange&simid=608048392595177497&selectedIndex=3
+    anima=Elemento(ing=ANIMAL,
+          tit="historias do mar",
+          style = dict(left=150,top=60,width=60,height=200))
+   cenaHawaii.vai()
+   historia()   
