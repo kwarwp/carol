ba5291d02711c167e6401400ad7b468da4e5820c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:22:46 -0300

    Saved angie/__log__.py at Timestamp: 2018-10-17 21:22:46

--- angie/__log__.py
+++ angie/__log__.py
@@ -41,3 +41,10 @@ SyntaxError: EOL while scanning string literal
                                                                                                                                                                                                                                                                                                                            ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Oct 17 2018 18:22:46.594 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  view=detailV2&ccid=RsqA2HeD&id=F1ACA62E553A23D0    cenaHawaii=Cena(ing="https://www.bing.com/images/search?C9E64DB3D4141F76208B938F&thid=OIP.RsqA2HeDbpYicvfQrURQ-QHaEK&mediaurl=https%3a%2f%2fi.ytimg.com%2fvi%2fL3V7LKYPIUQ%2fmaxresdefault.jpg&exph=720&expw=1280&q=hawaii+imgens&simid=608001031972457035&selectedIndex=0
+  ^
+IndentationError: expected an indented block
+'''},
