57e2da5864130a54636f167d59e3e8f6fa57eea0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:08:40 -0300

    Saved libby/main.py at Timestamp: 2018-10-17 21:08:40

--- libby/main.py
+++ libby/main.py
@@ -1,8 +1,13 @@
 #laura
 from _spy.vitollino.main import Cena,Elemento,Texto
 
-bunequinho ="http://56.media.tumblr.com/b29d0f586c54e40d71c8566efba0cecd/tumblr_o31t6fLBbo1tx4472o1_1280.png" 
+MALVADO ="http://56.media.tumblr.com/b29d0f586c54e40d71c8566efba0cecd/tumblr_o31t6fLBbo1tx4472o1_1280.png" 
 def Historia():
-    cenaCastelo = Cena(img= "http://i67.tinypic.com/f24z7l.jpg") 
+    cenaCastelo = Cena(img= "http://i67.tinypic.com/f24z7l.jpg")
+    malvado= Elemento(imgmalvado,
+           tit="malvado",
+           style = dict(left=150, top=60,width=60, height=200))
+    cenaCastelo.vai()
+           
 Historia()
 
