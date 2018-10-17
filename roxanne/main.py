49ce67c885993fb8a02b57a56b312201cd8f495d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 17:37:30 -0300

    Saved roxanne/main.py at Timestamp: 2018-10-17 20:37:29

--- roxanne/main.py
+++ roxanne/main.py
@@ -3,4 +3,5 @@ from _spy.vitollino.main import Cena,Elemento,Texto
 def Historia():
     cenaParque = Cena(img = "https://cdn.civitatis.com/argentina/buenos-aires/guia/parque-tres-febrero-grid-m.jpg")
     cenaParque.vai()
-Historia()
+Historia():
+colosus ='https://upload.wikimedia.org/wikipedia/az/b/bb/Colossus.png'
