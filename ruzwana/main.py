18af0f5a3e2cf613e7d10fb925323280c5dc0129
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 17:38:51 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-17 20:38:50

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -3,6 +3,6 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 
 girl="https://www.google.com.br/search?safe=strict&rlz=1C1SQJL_pt-BRBR818&biw=1366&bih=657&tbm=isch&sa=1&ei=A8G7W4SND5KQ_QbYzYeYDQ&q=anime+kawaii+neko&oq=anime+kaw&gs_l=img.1.6.0l4j0i67k1l2j0l4.147118.151008.0.153273.17.13.0.1.1.0.265.1698.0j7j2.9.0....0...1c.1.64.img..8.9.1458....0.TQfqXOdsz4s"
 def Historia():
-        CenaTeatro = Cena(img = "https://www.google.com.br/search?safe=strict&rlz=1C1SQJL_pt-BRBR818&biw=1366&bih=657&tbm=isch&sa=1&ei=-Mi7W-zfM-u6ggesxJeYAQ&q=cena+de+teatro+vazia&oq=cena+de+teatro+vazia&gs_l=img.3...366245.368651.0.369238.8.7.1.0.0.0.499.1073.2-1j1j1.3.0....0...1c.1.64.img..4.0.0....0.zwMQMeyK4N4" )
-        CenaTeatro=.vai()
+        CenaTeatro = Cena(img ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdPLYgHZx_3F32cbuvh1F1JCtI7wCETneHux09yVAqsmfEOX5C")
+        CenaTeatro.vai()
 Historia()
