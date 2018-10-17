7e4e4674829e077cc69af3609daddb17f1add36d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:30:13 -0300

    Saved angie/__log__.py at Timestamp: 2018-10-17 21:30:13

--- angie/__log__.py
+++ angie/__log__.py
@@ -62,3 +62,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Oct 17 2018 18:30:13.269 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  cenaHawaii=Cena(img="https://www.bing.com/images/search?view=detailV2&ccid=Y1T2pWEV&id=69E038ECD76C78D633FED0952F9EF3B9828180C8&thid=OIP.Y1T2pWEVQDV1V_NRIo78BAHaH3&mediaurl=https%3a%2f%2fst3.depositphotos.com%2f9237796%2f16585%2fi%2f1600%2fdepositphotos_165859096-stock-photo-tropical-hawaii-leaves-frame-in.jpg&exph=1700&expw=1600&q=wahaii+imagens+png&simid=608002487962437097&selectedIndex=24
+  ^
+IndentationError: expected an indented block
+'''},
