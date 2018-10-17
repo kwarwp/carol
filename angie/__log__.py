9fba73129378b2ba07d742ad8ca30ce3e31cad8a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:15:07 -0300

    Saved angie/__log__.py at Timestamp: 2018-10-17 21:15:07

--- angie/__log__.py
+++ angie/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: EOL while scanning string literal
                                                                                                                                                                                                                                                                                                                                                                                                                               ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Oct 17 2018 18:15:06.843 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  cenaHawaii=cena(ing="https://www.bing.com/images/search?view=detailV2&ccid=QKsRkE7z&id=B360AF9CE9DE6C5E8D5F80197473AD7C76CF7DB5&thid=OIP.QKsRkE7z20HKB01_DmOTzgHaFc&mediaurl=https%3a%2f%2fc8.alamy.com%2fcomppt%2fcxr4n2%2fcaptadores-de-conduzir-atraves-de-uma-manga-grove-big-island-hawaii-estados-unidos-da-america-cxr4n2.jpg&exph=956&expw=1300&q=hawaii+endereco+de+imange&simid=608048392595177497&selectedIndex=3
+                                                                                                                                                                                                                                                                                                                                                                                                                              ^
+SyntaxError: EOL while scanning string literal
+'''},
