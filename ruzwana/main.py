437eed83a849c11094768d36f753c2578f61e4be
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:30:35 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-17 21:30:35

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,12 +1,12 @@
 #Cibele
 from _spy.vitollino.main import Cena, Elemento, Texto 
 
-Chica="https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwinuPvVrI7eAhWKIZAKHQ7cDWsQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F53128470583809726%2F&psig=AOvVaw1Dx2WPQc_aO0Aom3IEpQ1H&ust=1539895692884798"
+Chica=
 def Historia():
-        CenaTeatro = Cena(img ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdPLYgHZx_3F32cbuvh1F1JCtI7wCETneHux09yVAqsmfEOX5C")
-        chica = Elemento(img=Chica,
+        cenaTeatro = Cena(img ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdPLYgHZx_3F32cbuvh1F1JCtI7wCETneHux09yVAqsmfEOX5C")
+        chica = Elemento(img =Chica,
                 tit ="Chica",
                 style = dict(left=150, top=60, width=60, height=200))
         chica.entra(cenaTeatro)
-        CenaTeatro.vai()
+        cenaTeatro.vai()
 Historia()
