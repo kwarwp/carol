c9b9a69c90453c08cdd7649c9dc8fe5a55486f9e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 17:30:37 -0300

    Saved roxanne/main.py at Timestamp: 2018-10-17 20:30:36

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,7 +1,5 @@
 #davy
 from _spy.vitollino.main import Cena,Elemento,Texto
-
-NINJA ="http://pngimg.com/uploads/ninja/ninja_PNG8.png"
 def Historia():
     cenaParque = Cena(img = "https://cdn.civitatis.com/argentina/buenos-aires/guia/parque-tres-febrero-grid-m.jpg")
     cenaParque.vai()
