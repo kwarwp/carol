e1dea40ede3405ed975be8fa43cb76d66e41f1ab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:28:09 -0300

    Created natalia/__log__.py at Timestamp: 2018-10-17 21:28:09

---
+++ natalia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Oct 17 2018 18:28:07.339 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  cenaFloresta=cena(img="https://upload.wikimedia.org/wikipedia/commons/f/f5/Floresta_de_Chantilly.JPG"
+                                                                                                             ^
+SyntaxError: invalid syntax
+'''},
