19ca15712f316908ff3074f8fa3d3ce6d4ae67e5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:12:42 -0300

    Saved kathryn/__log__.py at Timestamp: 2018-10-17 21:12:42

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -66,3 +66,10 @@ SyntaxError: invalid syntax
                                                                                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Oct 17 2018 18:12:43.450 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  powerranger = Elemento(img=PAWERRENGER,
+                                                                                                                                     ^
+SyntaxError: invalid syntax
+'''},
