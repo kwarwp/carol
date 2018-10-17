8523a30221042c02ef49938a2d5404e30b04ac23
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:10:44 -0300

    Saved roxanne/main.py at Timestamp: 2018-10-17 21:10:44

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,7 +1,12 @@
 #davy
 from _spy.vitollino.main import Cena,Elemento,Texto
+
+colosos ='https://upload.wikimedia.org/wikipedia/az/b/bb/Colossus.png'
 def Historia():
     cenaParque = Cena(img = "https://cdn.civitatis.com/argentina/buenos-aires/guia/parque-tres-febrero-grid-m.jpg")
-    cenaParque.vai()
+    colosos = Elemento(img=colosos,
+              tit ="colosos",
+              style = dict(left=150, top=60, width=60, height=200))
+colosos.entra(cenaPaque)   
+   cenaParque.vai()
 Historia():
-colosus ='https://upload.wikimedia.org/wikipedia/az/b/bb/Colossus.png'
