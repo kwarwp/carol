8ab8ebf6c25fcbd7d32348a84c657cd9891543a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 17:32:05 -0300

    Saved libby/main.py at Timestamp: 2018-10-17 20:32:04

--- libby/main.py
+++ libby/main.py
@@ -1,8 +1,8 @@
 #laura
-from _spy.vittoline.mainimport Cena,Elemento,Texto
+from _spy.vitollino.main import Cena,Elemento,Texto
 CASTELO ="http://i67.tinypic.com/f24z7l.jpg"
 bunequinho ="http://56.media.tumblr.com/b29d0f586c54e40d71c8566efba0cecd/tumblr_o31t6fLBbo1tx4472o1_1280.png" 
-def Historia()
-  cenaCastelo = Cena(img= "http://i67.tinypic.com/f24z7l.jpg") 
-Hiatoria()
+def Historia():
+    cenaCastelo = Cena(img= "http://i67.tinypic.com/f24z7l.jpg") 
+Historia()
 
