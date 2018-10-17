acb736f36b6ad23d3ce0706bf736131f153458ca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:20:49 -0300

    Saved parisa/main.py at Timestamp: 2018-10-17 21:20:48

--- parisa/main.py
+++ parisa/main.py
@@ -1,9 +1,4 @@
-#Arthur Correa  
-from _spy.vitollino.main import Cena, Elemento,Texto
 
-CACADOR="http://www.culturamix.com/wp-content/gallery/cacador-2/images.png"
-def Historia():
-    cenaFloresta=Cena(img="http://homeroreis.com/wp-content/uploads/2015/10/floresta.jpg")
     cacador = Elemento(ing=CACADOR,
               tit ="Caçador",
               style = dict(left=150, top=60, width=60, height=200))
