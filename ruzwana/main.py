4e147f842cf8a7441ee203b74f53428569a6ca68
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:08:47 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-17 21:08:47

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,8 +1,11 @@
 #Cibele
 from _spy.vitollino.main import Cena, Elemento, Texto 
 
-girl="https://www.google.com.br/search?safe=strict&rlz=1C1SQJL_pt-BRBR818&biw=1366&bih=657&tbm=isch&sa=1&ei=A8G7W4SND5KQ_QbYzYeYDQ&q=anime+kawaii+neko&oq=anime+kaw&gs_l=img.1.6.0l4j0i67k1l2j0l4.147118.151008.0.153273.17.13.0.1.1.0.265.1698.0j7j2.9.0....0...1c.1.64.img..8.9.1458....0.TQfqXOdsz4s"
+Chica="https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwinuPvVrI7eAhWKIZAKHQ7cDWsQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F53128470583809726%2F&psig=AOvVaw1Dx2WPQc_aO0Aom3IEpQ1H&ust=1539895692884798"
 def Historia():
         CenaTeatro = Cena(img ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdPLYgHZx_3F32cbuvh1F1JCtI7wCETneHux09yVAqsmfEOX5C")
+        chica = Elemento(img=Chica,
+                tit ="Chica",
+                style = dict(left=150, top=60, width=60, height=200))
         CenaTeatro.vai()
 Historia()
