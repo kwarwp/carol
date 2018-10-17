b2c05a2fa8140164606f6f41c44d0c7d60aca1a3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:16:05 -0300

    Saved grace/__log__.py at Timestamp: 2018-10-17 21:16:04

--- grace/__log__.py
+++ grace/__log__.py
@@ -17,3 +17,10 @@
     cenaCastle=Cena(img = "https://http2.mlstatic.com/fundo-fotografico-tecido-newborn-3d-castelo-150m-x-220m-D_NQ_NP_812472-MLB27642632585_062018-O.webp")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Wed Oct 17 2018 18:16:05.106 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  mario.entra(cenaCastle)
+  ^
+IndentationError: unexpected indent
+'''},
