2fc55a9365756f9a9c02b5383e1267572430f426
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:14:58 -0300

    Saved kathryn/main.py at Timestamp: 2018-10-17 21:14:58

--- kathryn/main.py
+++ kathryn/main.py
@@ -4,9 +4,9 @@ from _spy.vitollino.main import Cena, Elemento,Texto
 POWERRANGER="https://1.bp.blogspot.com/-mCl9tarfYd4/V5z6bLvJVWI/AAAAAAAAIAo/pgYvjjqTqn8oMXxLU_NQWyXQkIBg2kFkQCLcB/s1600/mighty_morphin__power_rangers__by_camo_flauge-da1l9zn.png"
 def Historia():
     cenaFloresta = Cena (img = "https://ogimg.infoglobo.com.br/boa-viagem/23123046-813-45f/GEOMIDIA/375/xwater-3092908_1920.jpg.pagespeed.ic.7q4TE_WU0u.jpg")
-    powerranger = Elemento(img=POWERRENGER,
-                   tit ="Power renger",
-                   style = dict(left=150, top=50,width=60, higth=200))
-    powerranger.entra(cena floresta)               
+    powerranger = Elemento (img=POWERRANGER,
+                  tit ="Power renger",
+                  style = dict(left=150, top=50,width=60, higth=100))
+    powerranger.entra(cenaFloresta)               
     cenaFloresta.vai()
 Historia()
