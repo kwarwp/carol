9185e7ee8e237c050c55c19818d1a6d48ff7b86e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:56:41 -0300

    Saved sara/main.py at Timestamp: 2018-10-19 14:56:40

--- sara/main.py
+++ sara/main.py
@@ -1,6 +1,11 @@
 #maibi
 from _spy.vittolino.main import Cena, Elemento, texto
 Hospital ="http://osshmtj.org.br/layout/imagens/h02.jpg"M
-Médico ="http://www.policlinicanovasaude.com.br/wp-content/uploads/2016/07/voce-medico.png"
+Medico ="http://www.policlinicanovasaude.com.br/wp-content/uploads/2016/07/voce-medico.png"
 
 def Historia():
+Ilha=Cena(img=
+zumbi9=Elemento(img=ZUMBI,tit="ZUMBI",style=dict(left=150,top=60,widt=60
+zumbi9.entra(ilha)
+Ilha.vai()
+Historia()
