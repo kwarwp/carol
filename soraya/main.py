7481a7d2879d4e320fcfc59476ae24d9eeffbfa9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:39:31 -0300

    Saved soraya/main.py at Timestamp: 2018-10-19 14:39:31

--- soraya/main.py
+++ soraya/main.py
@@ -1,10 +1,11 @@
 #matheusterra
 from _spy.vitollino.main import Cena, Elemento, Texto
 
-SUNAGAURE = "https://s.respostas.com.br/imagens/deserto.jpg"
+SUNAGAKURE = "https://s.respostas.com.br/imagens/deserto.jpg"
 def Historia ():
     DESERTO = Cena(img = "https://c1.staticflickr.com/4/3096/2913346696_9e9d1b3fa4.jpg")
-    DESERTO.vai()
-Historia()
     sunagakure = Elemento(img=SUNAGAKURE, tit="Sunagure", style=dict(left=150, top=60, width=60, height=200))
+    DESERTO.vai
+Historia()
+   
     
