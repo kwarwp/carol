6d06c542ec099e6297c658aec54ddcb6fc47496e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:58:00 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-19 14:58:00

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,16 +1,14 @@
 #Raphael
-from _spy.vitollino.main import Cena, Elemento, Texto 
+from _spy.vitollino.main import Cena, Elemento, Texto
 
 #Glub Glub
 
-mar = "https://mardehistorias.files.wordpress.com/2010/11/oceano.jpg"
-nemo = "http://www.araguaiaaquarios.com.br/loja/modules/possequence/images/image_1.jpg"
+
+NEMO ="http://www.araguaiaaquarios.com.br/loja/modules/possequence/images/image_1.jpg"
 
 def Historia():
-    cenamar = Cena(img="https://mardehistorias.files.wordpress.com/2010/11/oceano.jpg")
-    nemo= Elemento(img="http://www.araguaiaaquarios.com.br/loja/modules/possequence/images/image_1.jpg",
-                   tit="nemo",
-                   style=dict(left=180, left = 40, height = 40, width = 170))
+    mar = Cena(img="https://mardehistorias.files.wordpress.com/2010/11/oceano.jpg")
+    nemo = Elemento(img=NEMO,tit="NEMO",style=dict(left = 180, top = 40, height = 40, width = 170))
     nemo.entra(mar)
     mar.vai ()
 Historia()
