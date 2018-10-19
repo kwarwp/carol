badb45f53abe99cdba2ea933484bbfd849f6284a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:38:14 -0300

    Saved soraya/__log__.py at Timestamp: 2018-10-19 14:38:13

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -278,3 +278,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Fri Oct 19 2018 11:38:14.432 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  sunagakure = Elemento(img=SUNAGAKURE, tit="Sunagure", style=dict(left=150, top=60, width=60, height=200))
+  ^
+IndentationError: unexpected indent
+'''},
