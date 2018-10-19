f56b6f45cbbfc59bd92ce6fe1481116cd5f9fcf6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:45:46 -0300

    Saved amanda/main.py at Timestamp: 2018-10-19 14:45:45

--- amanda/main.py
+++ amanda/main.py
@@ -5,6 +5,6 @@ HOMEM = "https://1.bp.blogspot.com/-RilnuddcuWQ/Wc2BC-eYG8I/AAAAAAAABGs/tLwWjcNO
 def Historia():
     ARVORES = Cena(img = "https://pensamentoverde.com.br/wp-content/uploads/2016/11/1-arvore.jpg")
     homem = Elemento(img=HOMEM, tit="Homem", style=dict(left=150, top=60, width=60, height=200))
-    homen.entra(ARVORES)
+    homem.entra(ARVORES)
     ARVORES.vai()
 Historia()
