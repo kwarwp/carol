ee47a0226b3308a7f4ce18bba9b7b7d07807e40e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:50:10 -0300

    Saved kristen/main.py at Timestamp: 2018-10-19 14:50:10

--- kristen/main.py
+++ kristen/main.py
@@ -6,6 +6,7 @@ CAMELO ="https://cdn.pixabay.com/photo/2017/01/17/18/45/camel-1987672_960_720.pn
 def Historia():
     DESERTO = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/0/0c/Deserto_no_Cerrado.jpg")
     CAMELO = Elemento(img=CAMELO, tit="CAMELO", style=dict(left= 150, top=60, width=60, height=200))
+    CAMELO.entra(DESERTO)
     DESERTO.vai()
 Historia()    
 
