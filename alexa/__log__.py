4125213236801acbef58940562c034c3c8382452
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:32:23 -0300

    Created alexa/__log__.py at Timestamp: 2018-10-19 14:32:23

---
+++ alexa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Oct 19 2018 11:32:21.655 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  macaco=Elemento(img=macaco, tit="macaco", style=dict(left=150, top=60, width=60, height=200))
+  ^
+IndentationError: unexpected indent
+'''},
