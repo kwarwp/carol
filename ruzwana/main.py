80e91839d695f3d697fb64d6023ab312871c5c81
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 12:02:02 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-19 15:02:02

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -7,7 +7,7 @@ NEMO ="http://www.araguaiaaquarios.com.br/loja/modules/possequence/images/image_
 
 def Historia():
     mar = Cena(img="https://media-cdn.tripadvisor.com/media/photo-s/0f/03/76/ef/fundo-do-mar.jpg")
-    nemo = Elemento(img=NEMO,tit="NEMO",style=dict(left = 180, top = 40, height = 40, width = 170))
+    nemo = Elemento(img=NEMO,tit="NEMO",style=dict(left = 100, top = 120, height = 80, width = 130))
     nemo.entra(mar)
     mar.vai ()
 Historia()
