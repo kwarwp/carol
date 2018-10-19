fa7daa770810ab3e7145aa89f2b712f0e23b8972
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:44:09 -0300

    Saved amanda/__log__.py at Timestamp: 2018-10-19 14:44:09

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -17,3 +17,21 @@
     ARVORES = Cena(img = "https://pensamentoverde.com.br/wp-content/uploads/2016/11/1-arvore.jpg")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Fri Oct 19 2018 11:44:08.760 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 10
+    Historia()
+  module <module> line 8
+    homen.entra(ARVORES)
+NameError: name 'homen' is not defined
+'''},
