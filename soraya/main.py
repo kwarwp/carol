5a40646b16ef241146ff958a4927ebb0bcd33249
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:43:17 -0300

    Saved soraya/main.py at Timestamp: 2018-10-19 14:43:17

--- soraya/main.py
+++ soraya/main.py
@@ -6,6 +6,8 @@ def Historia ():
     DESERTO = Cena(img = "https://c1.staticflickr.com/4/3096/2913346696_9e9d1b3fa4.jpg")
     sunagakure = Elemento(img=SUNAGAKURE, tit="Sunagure", style=dict(left=150, top=60, width=60, height=200))
     DESERTO.vai
+    sunagakure.entra(DESERTO)
+    DESERTO.vai ()
 Historia()
    
     
