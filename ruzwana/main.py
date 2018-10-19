221accb2c2b9a00b077d2a2a24c727b5338bbe0f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 12:00:46 -0300

    Saved ruzwana/main.py at Timestamp: 2018-10-19 15:00:46

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -3,11 +3,10 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 
 #Glub Glub
 
-
 NEMO ="http://www.araguaiaaquarios.com.br/loja/modules/possequence/images/image_1.jpg"
 
 def Historia():
-    mar = Cena(img="https://mardehistorias.files.wordpress.com/2010/11/oceano.jpg")
+    mar = Cena(img="https://media-cdn.tripadvisor.com/media/photo-s/0f/03/76/ef/fundo-do-mar.jpg")
     nemo = Elemento(img=NEMO,tit="NEMO",style=dict(left = 180, top = 40, height = 40, width = 170))
     nemo.entra(mar)
     mar.vai ()
