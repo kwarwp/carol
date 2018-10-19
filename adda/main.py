2a228b43bca7874a7e20990312f1336f2e96a9c7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:43:35 -0300

    Saved adda/main.py at Timestamp: 2018-10-19 14:43:35

--- adda/main.py
+++ adda/main.py
@@ -6,5 +6,6 @@ SIRI ="https://i.pinimg.com/originals/ff/00/51/ff005199bfa12a152819f86b196a6037.
 def Historia():
     PRAIA = Cena(img = "https://www.fundospaisagens.com/Imagens/fundo-de-ecra-de-praia.jpg")
     siri = Elemento(img=SIRI, tit="Siri", style=dict(left=150, top=60, whidth=60, height=200))
+    siri.entra(PRAIA)
     PRAIA.vai()
 Historia()
