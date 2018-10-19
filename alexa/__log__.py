1cf12ec7118437ac19c4707521e45dcec17eb41d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-19 11:33:41 -0300

    Saved alexa/__log__.py at Timestamp: 2018-10-19 14:33:41

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Fri Oct 19 2018 11:33:41.158 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  macaco=Elemento(img=macaco, tit="macaco", style=dict(left=150, top=60, width=60, height=200))
+  ^
+IndentationError: unexpected indent
+'''},
