323ae9531084a9d948da4daace12d9ff6f7492ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 17:45:51 -0300

    Saved natalia/__log__.py at Timestamp: 2018-10-22 20:45:51

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -34,3 +34,10 @@ SyntaxError: invalid syntax
                                                                                                                                                                                                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Oct 22 2018 17:45:49.13 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  gokudeussupersayjin = Elemento(img = "https://2.bp.blogspot.com/-TLxp11naewU/WY3Ff6Iu6iI/AAAAAAAAIFE/r7yeQjVKXuYDzuDkBJ8q0oAC51Z58JnHgCLcBGAs/s1600/Goku%2BSuper%2BSayajin%2Bdeus.png"
+                                                                                                                                                                                                                  ^
+SyntaxError: invalid syntax
+'''},
