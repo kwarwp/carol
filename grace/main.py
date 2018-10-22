4a672bf10c307cf3721cdb00aa2ac511160e3a51
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 18:18:25 -0300

    Saved grace/main.py at Timestamp: 2018-10-22 21:18:25

--- grace/main.py
+++ grace/main.py
@@ -1,6 +1,6 @@
 #GabrielLucas e Brenno
 from _spy.vitollino.main import Cena, Elemento, Texto
-MarioBros= "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj9jJKM2ffdAhWMjFkKHZo8ApYQjRx6BAgBEAU&url=https%3A%2F%2Franktopten.com%2Ftop-super-smash-flash-2-characters-4665%2Fmario&psig=AOvVaw3YFWeWIA0rVCDDygbNlK1G&ust=1539116971073924"
+MarioBros= "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/MarioSMBW.png/220px-MarioSMBW.png"
 def Historia():
     cenaCastle = Cena(img = "https://http2.mlstatic.com/fundo-fotografico-tecido-newborn-3d-castelo-150m-x-220m-D_NQ_NP_812472-MLB27642632585_062018-O.webp")
     mario= Elemento(img=MarioBros,
