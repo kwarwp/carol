1be88e7183d696a8e2eebc3b54d25c8a744108a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 17:58:23 -0300

    Saved lisa/main.py at Timestamp: 2018-10-22 20:58:23

--- lisa/main.py
+++ lisa/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 
 GOKU ="https://i.ytimg.com/vi/jEjtNGZnsak/hqdefault.jpg"
 cenaTeatro = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
-    Goku = Elemento(img,
+    Goku = Elemento(img= GOKU,
            tit ="Goku",
            style = dict(left=150, top=60,width=60, height=200))
     goku.entra(cenaTeatro)
