7856ef429f4d714d97b8bdcfe5117b97943c4f63
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 17:36:34 -0300

    Saved natalia/main.py at Timestamp: 2018-10-22 20:36:34

--- natalia/main.py
+++ natalia/main.py
@@ -7,7 +7,8 @@ def Historia():
     cenaFloresta= Cena(img="https://upload.wikimedia.org/wikipedia/commons/f/f5/Floresta_de_Chantilly.JPG"
     gokudeussupersayjin = Elemento(img = "https://2.bp.blogspot.com/-TLxp11naewU/WY3Ff6Iu6iI/AAAAAAAAIFE/r7yeQjVKXuYDzuDkBJ8q0oAC51Z58JnHgCLcBGAs/s1600/Goku%2BSuper%2BSayajin%2Bdeus.png"
                         tit=" Goku Deus Super Sayjin",
-                        style=dict(left=150, top=100, width= 90, height= 200))
+                        style=dict(left=150, top=60, width= 60, height= 200)]
+                        
     gokudeussupersayjin.entra(cenafloresta)
     cenaFloresta.vai()                
 Historia()  
