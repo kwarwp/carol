8b2627f89ee62ec5114c81f05779827aacacbd8b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 18:14:52 -0300

    Saved natalia/main.py at Timestamp: 2018-10-22 21:14:52

--- natalia/main.py
+++ natalia/main.py
@@ -1,14 +1,14 @@
 #nathan
-from _spy.vittolino.main import Cena,Elemento, Texto
+from _spy.vittolino.main import Cena,Elemento,Texto
 
 GOKU="https://2.bp.blogspot.com/-TLxp11naewU/WY3Ff6Iu6iI/AAAAAAAAIFE/r7yeQjVKXuYDzuDkBJ8q0oAC51Z58JnHgCLcBGAs/s1600/Goku%2BSuper%2BSayajin%2Bdeus.png"
 
 def Historia():
-    cenaFloresta= Cena(img="https://upload.wikimedia.org/wikipedia/commons/f/f5/Floresta_de_Chantilly.JPG")
+    cenaFloresta=Cena(img="https://upload.wikimedia.org/wikipedia/commons/f/f5/Floresta_de_Chantilly.JPG")
     gokudeussupersayjin = Elemento(img = GOKU,
-                        tit= " Goku Deus Super Sayjin",
+                        tit= " goku deus super sayjin",
                         style=dict(left=150, top=60, width= 60, height= 200))
                         
-    gokudeussupersayjin.entra(cenafloresta)
+    gokudeussupersayjin.entra(cenaFloresta)
     cenaFloresta.vai()                
 Historia()  
