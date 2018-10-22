43f3d18e57cd8c0a465cf3d37d3a6c53b05bd7e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 17:39:00 -0300

    Saved callie/main.py at Timestamp: 2018-10-22 20:39:00

--- callie/main.py
+++ callie/main.py
@@ -1,7 +1,17 @@
 # carlos eduardo de oliveira assumpcao
-from _spy.vittolino.main import Cena, Elemento, Texto
-IMGOD ="http://3.bp.blogspot.com/-PSaNS37veEI/UqkOmMBXcvI/AAAAAAAAAIQ/Nqv0XHO5fKk/s1600/Sonic.exe4.jpg"
-SONICEXE ="http://pixelartmaker.com/art/5c608d29642e09e.png"
+#from _spy.vittolino.main import Cena, Elemento, Texto
+#IMGOD ="http://3.bp.blogspot.com/-PSaNS37veEI/UqkOmMBXcvI/AAAAAAAAAIQ/Nqv0XHO5fKk/s1600/Sonic.exe4.jpg"
+#SONICEXE ="http://pixelartmaker.com/art/5c608d29642e09e.png"
 
-def historia():
+#def historia():
+#    cena
+
+from _spy.vitollino.main import Cena, Elemento, Texto
+
+GOKU ="https://i.ytimg.com/vi/jEjtNGZnsak/hqdefault.jpg"
+cenaTeatro = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
+    Goku = Elemento(img,
+           tit ="Goku",
+           style = dict(left=150, top=60,width=60, height=200))
+    goku.entra(cenaTeatro)
     cena
