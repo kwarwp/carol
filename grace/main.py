da704d9c066d1f6f93b2c9159d73f250dccd51bf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 18:29:01 -0300

    Saved grace/main.py at Timestamp: 2018-10-22 21:29:00

--- grace/main.py
+++ grace/main.py
@@ -8,6 +8,6 @@ def Historia():
             style = dict(left=150, top=60, width=60, height=200))
     mario.entra(cenaCastle)
     txtmarioBros = Texto (cenaCastle,  "Hello")
-    txtmarioBros.vai=mario.vai
-    cenaCastle.vai()
+    txtmario.vai=mario.vai
+    cenaCastle.vai()a
 Historia()
