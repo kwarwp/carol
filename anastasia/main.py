cc0aff30242fd9ee9b9d85a2b99cfe40b54c6632
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-22 18:29:22 -0300

    Saved anastasia/main.py at Timestamp: 2018-10-22 21:29:22

--- anastasia/main.py
+++ anastasia/main.py
@@ -6,6 +6,7 @@ SAPO ="https://www.imagenspng.com.br/wp-content/uploads/2015/04/galinha-pintadin
 def Historia():
     CASTELO = Cena(img = "http://obviousmag.org/grifo/assets_c/2016/05/castelo-thumb-800x400-149789.jpg")
     sapo = Elemento(img=SAPO, tit="Sapo", style=dict(left=150, top=60, width=60, height=200))
+    cacador.entra[cena floresta]txtcacador
     CASTELO.vai()
 Historia()
     
