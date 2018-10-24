2c94af4cbd7fb0c27d937a8efef0eeda82d89152
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 18:01:49 -0300

    Saved courtney/main.py at Timestamp: 2018-10-24 21:01:48

--- courtney/main.py
+++ courtney/main.py
@@ -3,7 +3,7 @@
 from _spy.vitollino.main import Cena,Elemento,Texto
 PERSONAGEM ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjmwdSo2PfdAhVtuVkKHdWDAMoQjRx6BAgBEAU&url=http%3A%2F%2Flife-writer.tumblr.com%2Fpost%2F123761609710%2Fi-missed-my-kids&psig=AOvVaw1kWh3XpDoI0P2G30m_UH6o&ust=1539116915250785.png"
 def Historia():
-    cenaSea = Cena(img = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHltyB3vfdAhWOzlkKHR49B8gQjRx6BAgBEAU&url=http%3A%2F%2Fpt-br.contos-de-hora-de-aventura.wikia.com%2Fwiki%2FArquivo%3A294392_Papel-de-Parede-Castelo-Doce-Hora-da-Aventura_1920x1080.jpg&psig=AOvVaw3Csk-jnuzvEvpqP_P6cRev&ust=1539118413301038")
+    cenaSea = Cena(img = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjH8oXQ_J_eAhWCkJAKHQTIAg8QjRx6BAgBEAU&url=https%3A%2F%2Fpixabay.com%2Fpt%2Ffloresta-%25C3%25A1rvores-plantas-ecologia-148727%2F&psig=AOvVaw0BTrUAOeOZQqkjv_b3oqGo&ust=1540501280689184.png")
     personagem=Elemento(img=PERSONAGEM,
                tit="historias da neve",
                style = dict(left=150,top=60,width=60,height=200))
