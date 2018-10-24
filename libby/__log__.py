7d577016d553cdc7a3a9732719bbc86302dce59d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 17:18:15 -0300

    Saved libby/__log__.py at Timestamp: 2018-10-24 20:18:15

--- libby/__log__.py
+++ libby/__log__.py
@@ -129,3 +129,21 @@ UnboundLocalError: local variable 'malvado' referenced before assignment
     malvado= Elemento(img=malvado,
 UnboundLocalError: local variable 'malvado' referenced before assignment
 '''},
+{'date': 'Wed Oct 24 2018 17:18:14.949 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    Historia()
+  module <module> line 7
+    malvado= Elemento(imgmalvado,
+NameError: name 'imgmalvado' is not defined
+'''},
