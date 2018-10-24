48f6f0f3271975b2172cf71ea134ab113280d829
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 17:35:15 -0300

    Saved kellee/main.py at Timestamp: 2018-10-24 20:35:15

--- kellee/main.py
+++ kellee/main.py
@@ -5,9 +5,9 @@ def Historia():
     cenaSea = Cena(img = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHltyB3vfdAhWOzlkKHR49B8gQjRx6BAgBEAU&url=http%3A%2F%2Fpt-br.contos-de-hora-de-aventura.wikia.com%2Fwiki%2FArquivo%3A294392_Papel-de-Parede-Castelo-Doce-Hora-da-Aventura_1920x1080.jpg&psig=AOvVaw3Csk-jnuzvEvpqP_P6cRev&ust=1539118413301038")
     personagem = Elemento(img= PERSONAGEM,
                  tit ="Personagem",
-                 atyle = dict(left=150, top=60, width=60, height= 200))
+                 style = dict(left=150, top=60, width=60, height= 200))
     personagem.entra(cenaSea)
-    txtpersonagem = Texto (cenaSea, "Hello"
+    txtpersonagem = Texto (cenaSea, "Hello")
     personagem.vai = txtpersonagem.vai
     cenaSea.vai
 Historia()
