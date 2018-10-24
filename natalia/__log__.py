897ad5a2fed9c1f7d0d29008c7d21ef2c15ddd7e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 17:25:13 -0300

    Saved natalia/__log__.py at Timestamp: 2018-10-24 20:25:13

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -174,3 +174,21 @@ TypeError: 'module' object is not callable
     cenaFloresta=Cena(img="https://upload.wikimedia.org/wikipedia/commons/f/f5/Floresta_de_Chantilly.JPG")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Wed Oct 24 2018 17:25:14.517 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    Historia()  
+  module <module> line 7
+    cenaFloresta=Cena(img="https://upload.wikimedia.org/wikipedia/commons/f/f5/Floresta_de_Chantilly.JPG")
+TypeError: 'module' object is not callable
+'''},
