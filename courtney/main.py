af1a1d1ae760f3647a6ea35afb872cc18f018562
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 18:21:07 -0300

    Saved courtney/main.py at Timestamp: 2018-10-24 21:21:07

--- courtney/main.py
+++ courtney/main.py
@@ -1,7 +1,7 @@
 # carol.courtney.main.py
 # yasmin.
 from _spy.vitollino.main import Cena,Elemento,Texto
-PERSONAGEM ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjmwdSo2PfdAhVtuVkKHdWDAMoQjRx6BAgBEAU&url=http%3A%2F%2Flife-writer.tumblr.com%2Fpost%2F123761609710%2Fi-missed-my-kids&psig=AOvVaw1kWh3XpDoI0P2G30m_UH6o&ust=1539116915250785.png"
+PERSONAGEM ="http://3.bp.blogspot.com/-EUuOvz2jl9w/T43K0qeYjFI/AAAAAAAAAOc/OcDgvqPOn64/s1600/3.png"
 def Historia():
     cenaSea = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Butchardgardens.jpg/1200px-Butchardgardens.jpg")
     personagem=Elemento(img=PERSONAGEM,
