159717a20a1e256d9a321055fcf8a358e91baebc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 17:27:08 -0300

    Saved lisa/main.py at Timestamp: 2018-10-24 20:27:08

--- lisa/main.py
+++ lisa/main.py
@@ -1,14 +1,14 @@
 #Carol 
 from _spy.vitollino.main import Cena, Elemento, Texto
 
-GOKU ="https://i.ytimg.com/vi/jEjtNGZnsak/hqdefault.jpg"
+GOKU ="http://www.stickpng.com/assets/images/584e83616a5ae41a83ddee39.png"
 def Historia():
     cenaTeatro = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
     goku = Elemento(img= GOKU,
            tit ="Goku",
-           style = dict(left=150, top=60,width=60, height=200))
+           style = dict(left=150, top=60,width=200, height=50))
     goku.entra(cenaTeatro)
-    txtgoku = Texto (cenaTeatro, "Hello"),
-    goku .vai = txtgoku .vai
+    txtgoku = Texto (cenaTeatro, "Hello")
+    goku.vai = txtgoku.vai
     cenaTeatro.vai()
 Historia()
