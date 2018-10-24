e10da5aeac5214b9c5515de87e0c1a0a2a7bac8b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 17:59:23 -0300

    Saved angie/main.py at Timestamp: 2018-10-24 20:59:23

--- angie/main.py
+++ angie/main.py
@@ -1,12 +1,14 @@
 # bruna de sa
-from _spy.vittolino.main import Cena,Elemento,Texto
+from _spy.vitollino.main import Cena, Elemento,Texto
 
-HAWAII ="https://cdn.pixabay.com/photo/2017/06/10/11/16/landscape-2389694_960_720.jpg"
-def historia():
-cenaHawaii=Cena(ing="https://www.bing.com/images/search?view=detailV2&ccid=Y1T2pWEV&id=69E038ECD76C78D633FED0952F9EF3B9828180C8&thid=OIP.Y1T2pWEVQDV1V_NRIo78BAHaH3&mediaurl=https%3a%2f%2fst3.depositphotos.com%2f9237796%2f16585%2fi%2f1600%2fdepositphotos_165859096-stock-photo-tropical-hawaii-leaves-frame-in.jpg&exph=1700&expw=1600&q=wahaii+imagens+png&simid=608002487962437097&selectedIndex=24
-    anima=Elmento(ing=ANIMAL,
-          tit="historias do mar",
-          style = dict(left=150,top=60,width=60,height=200))
-   animal.entra(cenaHawaii)
-   cenaHawaii.vai()
-   historia()   
+CACADOR="https://2.bp.blogspot.com/-proH1UNc9Yw/V1ShJQ0HsMI/AAAAAAAAGF4/dSBPvEvXwDYFgEi1jLNlIcnc6JwKhmtawCLcB/s1600/o-ca%25C3%25A7ador-e-a-rainha-do-gelo-poshhterk.png"
+def Historia():
+    cenaFloresta=Cena(img="http://homeroreis.com/wp-content/uploads/2015/10/floresta.jpg")
+    cacador = Elemento(img= CACADOR,
+              tit ="Caçador",
+              style = dict(left=150, top=60, width=60, height=200))
+    cacador.entra(cenaFloresta)
+    txtcacador = Texto (cenaFloresta, "Hello")
+    cacador.vai = txtcacador.vai
+    cenaFloresta.vai()
+Historia()
