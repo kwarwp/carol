4206bdfaca5942dc7ce61071c8b09738c658c00c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 18:22:59 -0300

    Saved courtney/main.py at Timestamp: 2018-10-24 21:22:58

--- courtney/main.py
+++ courtney/main.py
@@ -3,12 +3,12 @@
 from _spy.vitollino.main import Cena,Elemento,Texto
 PERSONAGEM ="http://3.bp.blogspot.com/-EUuOvz2jl9w/T43K0qeYjFI/AAAAAAAAAOc/OcDgvqPOn64/s1600/3.png"
 def Historia():
-    cenaSea = Cena(img = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Butchardgardens.jpg/1200px-Butchardgardens.jpg")
+    cenaSea = Cena(img = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHltyB3vfdAhWOzlkKHR49B8gQjRx6BAgBEAU&url=http%3A%2F%2Fpt-br.contos-de-hora-de-aventura.wikia.com%2Fwiki%2FArquivo%3A294392_Papel-de-Parede-Castelo-Doce-Hora-da-Aventura_1920x1080.jpg&psig=AOvVaw3Csk-jnuzvEvpqP_P6cRev&ust=1539118413301038")
     personagem=Elemento(img=PERSONAGEM,
                tit="historias da neve",
                style = dict(left=150,top=60,width=60,height=200))
     personagem.entra(cenaSea)
     txtpersonagem = Texto (cenaSea,"Hello")
-    personagem.vai=txtpersonagem.vai
-    cenaSea.vai()
+    personagem.vai()
+    cenaSea.vai
 Historia()   
