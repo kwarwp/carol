ba200d174c8728684fc2dbd5e0250df93b78e227
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 17:56:46 -0300

    Saved libby/__log__.py at Timestamp: 2018-10-24 20:56:46

--- libby/__log__.py
+++ libby/__log__.py
@@ -237,3 +237,21 @@ NameError: name 'cenacastelo' is not defined
     malvado.entra(cenacastelo)
 NameError: name 'cenacastelo' is not defined
 '''},
+{'date': 'Wed Oct 24 2018 17:56:46.33 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    Historia()
+  module <module> line 11
+    txtmalvado = Texto (cenacastelo)
+NameError: name 'cenacastelo' is not defined
+'''},
