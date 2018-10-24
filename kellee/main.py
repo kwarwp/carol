2b4e76f32f2b1058254a7c0b6d0c632a2f9a22c8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 17:30:28 -0300

    Saved kellee/main.py at Timestamp: 2018-10-24 20:30:27

--- kellee/main.py
+++ kellee/main.py
@@ -1,7 +1,13 @@
-# vitori
+# vitoria
 from _spy.vitollino.main import Cena,Elemento,Texto
 PERSONAGEM ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjmwdSo2PfdAhVtuVkKHdWDAMoQjRx6BAgBEAU&url=http%3A%2F%2Flife-writer.tumblr.com%2Fpost%2F123761609710%2Fi-missed-my-kids&psig=AOvVaw1kWh3XpDoI0P2G30m_UH6o&ust=1539116915250785.png"
 def Historia():
     cenaSea = Cena(img = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHltyB3vfdAhWOzlkKHR49B8gQjRx6BAgBEAU&url=http%3A%2F%2Fpt-br.contos-de-hora-de-aventura.wikia.com%2Fwiki%2FArquivo%3A294392_Papel-de-Parede-Castelo-Doce-Hora-da-Aventura_1920x1080.jpg&psig=AOvVaw3Csk-jnuzvEvpqP_P6cRev&ust=1539118413301038")
+    personagem = Elemento(img= PERSONAGEM,
+                 tit ="Personagem",
+                 atyle = dict(left=150, top=60, width=60, height= 200))
+    personagem.entra(cenaSea)
+    txtpersonagem = Texto (cenaSea, "Hello"
+    personagem.vai = txtpersonagem.vai
     cenaSea.vai
 Historia()
