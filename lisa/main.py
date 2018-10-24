cfef9c398ce326acb3309125a8b68e7ff4bdac37
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 18:03:11 -0300

    Saved lisa/main.py at Timestamp: 2018-10-24 21:03:10

--- lisa/main.py
+++ lisa/main.py
@@ -6,7 +6,7 @@ def Historia():
     cenaTeatro = Cena(img ="http://1.bp.blogspot.com/-aGwacYjRKw8/T-St9kEVvMI/AAAAAAAAAvs/ximTg6O8XUA/s1600/ESCENARIOS29+%282%.jpg")
     goku = Elemento(img= GOKU,
            tit ="Goku",
-           style = dict(left=150, top=60,width=200, height=50))
+           style = dict(left=100, top=60,width=200, height=45))
     goku.entra(cenaTeatro)
     txtgoku = Texto (cenaTeatro, "Hello")
     goku.vai = txtgoku.vai
