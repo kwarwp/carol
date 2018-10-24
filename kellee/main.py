cb15039f8376cd01573f92389cd79949f62c8f36
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-24 17:42:46 -0300

    Saved kellee/main.py at Timestamp: 2018-10-24 20:42:45

--- kellee/main.py
+++ kellee/main.py
@@ -1,6 +1,6 @@
 # vitoria
 from _spy.vitollino.main import Cena,Elemento,Texto
-PERSONAGEM ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjmwdSo2PfdAhVtuVkKHdWDAMoQjRx6BAgBEAU&url=http%3A%2F%2Flife-writer.tumblr.com%2Fpost%2F123761609710%2Fi-missed-my-kids&psig=AOvVaw1kWh3XpDoI0P2G30m_UH6o&ust=1539116915250785.png"
+PERSONAGEM ="https://images.vexels.com/media/users/3/130955/isolated/preview/08c00870e43aa206e86e5f3b3495e902-personagem-de-desenho-animado-da-menina-hipster-by-vexels.png"
 def Historia():
     cenaSea = Cena(img = "https://media-cdn.tripadvisor.com/media/photo-s/11/56/f7/65/grande-teatro-palacio.jpg")
     personagem = Elemento(img= PERSONAGEM,
@@ -9,5 +9,5 @@ def Historia():
     personagem.entra(cenaSea)
     txtpersonagem = Texto (cenaSea, "Hello")
     personagem.vai = txtpersonagem.vai
-    cenaSea.vai
+    cenaSea.vai()
 Historia()
